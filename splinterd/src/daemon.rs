bc7a732d8f652392890e31af3a530e6bf8a0324d
Peter Schwarz <pschwarz@bitwise.io>
2019-02-09 13:43:23 -0600

    Change network set to take str reference
    Change the public API of Network.send to take a `&str`, which allows for
    callers to avoid clones when logging the peer id on usage.  The
    implementation hides an effective clone, which can be optimized by
    future improvements to the bimap.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- splinterd/src/daemon.rs
+++ splinterd/src/daemon.rs
@@ -190,7 +190,7 @@ impl SplinterDaemon {
         let connect_request_msg_bytes = create_connect_request()?;
         for peer_id in self.network.peer_ids() {
             debug!("Sending connect request to peer {}", peer_id);
-            self.network.send(peer_id, &connect_request_msg_bytes)?;
+            self.network.send(&peer_id, &connect_request_msg_bytes)?;
         }
 
         // For each node in the circuit_directory, try to connect and add them to the network
