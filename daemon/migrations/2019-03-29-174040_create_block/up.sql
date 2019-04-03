3fa2ae0cfa834e2cd0f37cd73b5febad173e143c
Peter Schwarz <pschwarz@bitwise.io>
2019-04-03 14:03:10 -0500

    Add initial migrations
    Add the initial tables needed for diesel migrations and the block table.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

---
+++ daemon/migrations/2019-03-29-174040_create_block/up.sql
@@ -0,0 +1,23 @@
+-- Copyright 2019 Cargill Incorporated
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- -----------------------------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS block (
+    block_id varchar(128) CONSTRAINT pk_block_id PRIMARY KEY,
+    block_num bigint NOT NULL,
+    state_root_hash varchar(64) NOT NULL
+);
+
+CREATE INDEX IF NOT EXISTS block_num_idx
+    ON block (block_num);
