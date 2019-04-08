4f330f0d7da606ddc6dd4fb827afb3536374088e
Andrea Gunderson <agunde@bitwise.io>
2019-04-08 07:47:41 -0500

    Add Grid Schema Smart contract
    This Commit implements the Smart contract described in
    the Grid Primitives RFC
    https://github.com/hyperledger/grid-rfcs/pull/4
    
    Note this does not include the permission check of if
    the agent has the permission can_create_schema for the
    organization. This we will be added in a separate PR
    after the implementation of Permission checker in the
    Grid SDK.
    
    Signed-off-by: Andrea Gunderson <agunde@bitwise.io>

---
+++ contracts/schema/src/payload.rs
@@ -0,0 +1,210 @@
+// Copyright 2019 Cargill Incorporated
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+cfg_if! {
+    if #[cfg(target_arch = "wasm32")] {
+        use sabre_sdk::ApplyError;
+    } else {
+        use sawtooth_sdk::processor::handler::ApplyError;
+    }
+}
+
+use grid_sdk::protocol::schema::payload::{
+    Action, SchemaCreateAction, SchemaPayload, SchemaUpdateAction,
+};
+
+pub fn validate_payload(payload: &SchemaPayload) -> Result<(), ApplyError> {
+    match payload.action() {
+        Action::SchemaCreate => validate_schema_create_action(payload.schema_create()),
+        Action::SchemaUpdate => validate_schema_update_action(payload.schema_update()),
+    }
+}
+
+fn validate_schema_create_action(create_action: &SchemaCreateAction) -> Result<(), ApplyError> {
+    if create_action.schema_name().is_empty() {
+        return Err(ApplyError::InvalidTransaction(String::from(
+            "Schema name must be set",
+        )));
+    }
+
+    if create_action.properties().is_empty() {
+        return Err(ApplyError::InvalidTransaction(String::from(
+            "Properties must not be empty",
+        )));
+    }
+    Ok(())
+}
+
+fn validate_schema_update_action(update_action: &SchemaUpdateAction) -> Result<(), ApplyError> {
+    if update_action.schema_name().is_empty() {
+        return Err(ApplyError::InvalidTransaction(String::from(
+            "Schema name must be set",
+        )));
+    }
+
+    if update_action.properties().is_empty() {
+        return Err(ApplyError::InvalidTransaction(String::from(
+            "Properties must not be empty",
+        )));
+    }
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use grid_sdk::protocol::schema::payload::{
+        Action, SchemaCreateBuilder, SchemaPayloadBuilder, SchemaUpdateBuilder,
+    };
+    use grid_sdk::protocol::schema::state::{DataType, PropertyDefinitionBuilder};
+    use grid_sdk::protos;
+    use grid_sdk::protos::IntoNative;
+
+    #[test]
+    // Test a payload with a schema create action is properly validated. This test needs to use
+    // the proto directly originally to be able to mimic the scenarios possbile from creating
+    // a SchemaPayload from bytes. This is because the SchemaPayloadBuilder protects from building
+    // a certain invalid payloads.
+    fn test_validate_schema_create_action() {
+        let mut payload_proto = protos::schema_payload::SchemaPayload::new();
+        assert!(
+            payload_proto.clone().into_native().is_err(),
+            "Cannot convert SchemaPayload_Action with type unset."
+        );
+
+        // create payload with no create SchemaCreateAction
+        payload_proto.set_action(protos::schema_payload::SchemaPayload_Action::SCHEMA_CREATE);
+        let payload = payload_proto.clone().into_native().unwrap();
+        assert!(
+            validate_payload(&payload).is_err(),
+            "Empty SchemaCreateAction should not be valid"
+        );
+
+        let mut action = protos::schema_payload::SchemaCreateAction::new();
+
+        // create payload with empty create SchemaCreateAction
+        payload_proto.set_schema_create(action.clone());
+        let payload = payload_proto.clone().into_native().unwrap();
+        assert!(
+            validate_payload(&payload).is_err(),
+            "Schema name must be set"
+        );
+
+        // create payload with SchemaCreateAction without any properites,
+        action.set_schema_name("test_schema".into());
+        payload_proto.set_schema_create(action.clone());
+        let payload = payload_proto.clone().into_native().unwrap();
+        assert!(
+            validate_payload(&payload).is_err(),
+            "Properties must not be empty"
+        );
+
+        // create payload with full payload
+        let builder = PropertyDefinitionBuilder::new();
+        let property_definition = builder
+            .with_name("TEST".to_string())
+            .with_data_type(DataType::String)
+            .with_description("Optional".to_string())
+            .build()
+            .unwrap();
+
+        let builder = SchemaCreateBuilder::new();
+        let action = builder
+            .with_schema_name("TestSchema".to_string())
+            .with_properties(vec![property_definition.clone()])
+            .build()
+            .unwrap();
+
+        let builder = SchemaPayloadBuilder::new();
+        let payload = builder
+            .with_action(Action::SchemaCreate)
+            .with_schema_create(action.clone())
+            .build()
+            .unwrap();
+
+        assert!(
+            validate_payload(&payload).is_ok(),
+            "Payload should be valid"
+        );
+    }
+
+    #[test]
+    // Test a payload with a schema update action is properly validated. This test needs to use
+    // the proto directly originally to be able to mimic the scenarios possbile from creating
+    // a SchemaPayload from bytes. This is because the SchemaPayloadBuilder protects from building
+    // a certain invalid payloads.
+    fn test_validate_schema_update_action() {
+        let mut payload_proto = protos::schema_payload::SchemaPayload::new();
+        assert!(
+            payload_proto.clone().into_native().is_err(),
+            "Cannot convert SchemaPayload_Action with type unset."
+        );
+
+        // create payload with no create SchemaCreateAction
+        payload_proto.set_action(protos::schema_payload::SchemaPayload_Action::SCHEMA_UPDATE);
+        let payload = payload_proto.clone().into_native().unwrap();
+        assert!(
+            validate_payload(&payload).is_err(),
+            "Empty SchemaUpdateAction should not be valid"
+        );
+
+        let mut action = protos::schema_payload::SchemaUpdateAction::new();
+
+        // create payload with empty create SchemaCreateAction
+        payload_proto.set_schema_update(action.clone());
+        let payload = payload_proto.clone().into_native().unwrap();
+        assert!(
+            validate_payload(&payload).is_err(),
+            "Schema name must be set"
+        );
+
+        // create payload with SchemaCreateAction without any properites,
+        action.set_schema_name("test_schema".into());
+        payload_proto.set_schema_update(action.clone());
+        let payload = payload_proto.clone().into_native().unwrap();
+        assert!(
+            validate_payload(&payload).is_err(),
+            "Properties must not be empty"
+        );
+
+        // create payload with full payload
+        let builder = PropertyDefinitionBuilder::new();
+        let property_definition = builder
+            .with_name("TEST".to_string())
+            .with_data_type(DataType::String)
+            .with_description("Optional".to_string())
+            .build()
+            .unwrap();
+
+        let builder = SchemaUpdateBuilder::new();
+        let action = builder
+            .with_schema_name("TestSchema".to_string())
+            .with_properties(vec![property_definition.clone()])
+            .build()
+            .unwrap();
+
+        let builder = SchemaPayloadBuilder::new();
+        let payload = builder
+            .with_action(Action::SchemaUpdate)
+            .with_schema_update(action.clone())
+            .build()
+            .unwrap();
+
+        assert!(
+            validate_payload(&payload).is_ok(),
+            "Payload should be valid"
+        );
+    }
+}
---
+++ contracts/schema/src/state.rs
@@ -0,0 +1,419 @@
+// Copyright 2019 Cargill Incorporated
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crypto::digest::Digest;
+use crypto::sha2::Sha512;
+use grid_sdk::protocol::pike::state::{Agent, AgentList, Organization, OrganizationList};
+use grid_sdk::protocol::schema::state::{Schema, SchemaList, SchemaListBuilder};
+use grid_sdk::protos::{FromBytes, IntoBytes};
+
+cfg_if! {
+    if #[cfg(target_arch = "wasm32")] {
+        use sabre_sdk::ApplyError;
+        use sabre_sdk::TransactionContext;
+    } else {
+        use sawtooth_sdk::processor::handler::ApplyError;
+        use sawtooth_sdk::processor::handler::TransactionContext;
+    }
+}
+
+pub const GRID_NAMESPACE: &str = "621dee";
+pub const GRID_SCHEMA_NAMESPACE: &str = "01";
+
+pub const PIKE_NAMESPACE: &str = "cad11d";
+pub const PIKE_AGENT_NAMESPACE: &str = "00";
+pub const PIKE_ORG_NAMESPACE: &str = "01";
+
+/// Computes the address a Pike Agent is stored at based on its public_key
+pub fn compute_agent_address(public_key: &str) -> String {
+    let mut sha = Sha512::new();
+    sha.input(public_key.as_bytes());
+
+    String::from(PIKE_NAMESPACE) + PIKE_AGENT_NAMESPACE + &sha.result_str()[..62].to_string()
+}
+
+/// Computes the address a Pike Organization is stored at based on its name
+pub fn compute_org_address(name: &str) -> String {
+    let mut sha = Sha512::new();
+    sha.input(name.as_bytes());
+
+    String::from(PIKE_NAMESPACE) + PIKE_ORG_NAMESPACE + &sha.result_str()[..62].to_string()
+}
+
+/// Computes the address a Grid Schema is stored at based on its name
+pub fn compute_schema_address(name: &str) -> String {
+    let mut sha = Sha512::new();
+    sha.input(name.as_bytes());
+
+    String::from(GRID_NAMESPACE) + GRID_SCHEMA_NAMESPACE + &sha.result_str()[..62].to_string()
+}
+
+/// GridSchemaState is in charge of handling getting and setting state.
+pub struct GridSchemaState<'a> {
+    context: &'a mut dyn TransactionContext,
+}
+
+impl<'a> GridSchemaState<'a> {
+    pub fn new(context: &'a mut dyn TransactionContext) -> GridSchemaState {
+        GridSchemaState { context }
+    }
+
+    /// Gets a Pike Agent. Handles retrieving the correct agent from an AgentList.
+    pub fn get_agent(&mut self, public_key: &str) -> Result<Option<Agent>, ApplyError> {
+        let address = compute_agent_address(public_key);
+        let d = self.context.get_state_entry(&address)?;
+        match d {
+            Some(packed) => {
+                let agents: AgentList = match AgentList::from_bytes(packed.as_slice()) {
+                    Ok(agents) => agents,
+                    Err(err) => {
+                        return Err(ApplyError::InvalidTransaction(format!(
+                            "Cannot deserialize agent list: {:?}",
+                            err,
+                        )));
+                    }
+                };
+
+                // find the agent with the correct public_key
+                for agent in agents.agents() {
+                    if agent.public_key() == public_key {
+                        return Ok(Some(agent.clone()));
+                    }
+                }
+                Ok(None)
+            }
+            None => Ok(None),
+        }
+    }
+
+    /// Gets a Pike Organization. Handles retrieving the correct organization from an
+    /// OrganizationList.
+    pub fn get_organization(&mut self, id: &str) -> Result<Option<Organization>, ApplyError> {
+        let address = compute_org_address(id);
+        let d = self.context.get_state_entry(&address)?;
+        match d {
+            Some(packed) => {
+                let orgs: OrganizationList = match OrganizationList::from_bytes(packed.as_slice()) {
+                    Ok(orgs) => orgs,
+                    Err(err) => {
+                        return Err(ApplyError::InvalidTransaction(format!(
+                            "Cannot deserialize organization list: {:?}",
+                            err,
+                        )));
+                    }
+                };
+
+                // find the organization with the correct org id
+                for org in orgs.organizations() {
+                    if org.org_id() == id {
+                        return Ok(Some(org.clone()));
+                    }
+                }
+                Ok(None)
+            }
+            None => Ok(None),
+        }
+    }
+
+    /// Gets a Grid Schema. Handles retrieving the correct Schema from a SchemaList
+    pub fn get_schema(&mut self, name: &str) -> Result<Option<Schema>, ApplyError> {
+        let address = compute_schema_address(name);
+        let d = self.context.get_state_entry(&address)?;
+        match d {
+            Some(packed) => {
+                let schemas: SchemaList = match SchemaList::from_bytes(packed.as_slice()) {
+                    Ok(schemas) => schemas,
+                    Err(err) => {
+                        return Err(ApplyError::InvalidTransaction(format!(
+                            "Cannot deserialize organization list: {:?}",
+                            err,
+                        )));
+                    }
+                };
+
+                // find the schema with the correct name
+                for schema in schemas.schemas() {
+                    if schema.name() == name {
+                        return Ok(Some(schema.clone()));
+                    }
+                }
+                Ok(None)
+            }
+            None => Ok(None),
+        }
+    }
+
+    /// Sets a Grid Schema in state. Handles creating a SchemaList if one does not already exist
+    /// at the address the schema will be stored. If a SchemaList does already exist, there has
+    /// been a hash collision. The Schema is stored in the SchemaList, sorted by the Schema name,
+    /// and set in state.
+    pub fn set_schema(&mut self, name: &str, new_schema: Schema) -> Result<(), ApplyError> {
+        let address = compute_schema_address(name);
+        let d = self.context.get_state_entry(&address)?;
+        // get list of existing schemas, or an empty vec if none
+        let mut schemas = match d {
+            Some(packed) => match SchemaList::from_bytes(packed.as_slice()) {
+                Ok(schema_list) => schema_list.schemas().to_vec(),
+                Err(err) => {
+                    return Err(ApplyError::InvalidTransaction(format!(
+                        "Cannot deserialize schema list: {}",
+                        err,
+                    )));
+                }
+            },
+            None => vec![],
+        };
+
+        // remove old schema if it exists and sort the schemas by name
+        let mut index = None;
+        for (count, schema) in schemas.iter().enumerate() {
+            if schema.name() == name {
+                index = Some(count);
+                break;
+            }
+        }
+
+        if let Some(x) = index {
+            schemas.remove(x);
+        }
+        schemas.push(new_schema);
+        schemas.sort_by_key(|s| s.name().to_string());
+
+        // build new SchemaList and set in state
+        let schema_list = SchemaListBuilder::new()
+            .with_schemas(schemas)
+            .build()
+            .map_err(|_| {
+                ApplyError::InvalidTransaction(String::from("Cannot build schema list"))
+            })?;
+
+        let serialized = match schema_list.into_bytes() {
+            Ok(serialized) => serialized,
+            Err(_) => {
+                return Err(ApplyError::InvalidTransaction(String::from(
+                    "Cannot serialize schema list",
+                )));
+            }
+        };
+        self.context
+            .set_state_entry(address, serialized)
+            .map_err(|err| ApplyError::InvalidTransaction(format!("{}", err)))?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::cell::RefCell;
+    use std::collections::HashMap;
+
+    use grid_sdk::protocol::pike::state::{
+        AgentBuilder, AgentListBuilder, OrganizationBuilder, OrganizationListBuilder,
+    };
+    use grid_sdk::protocol::schema::state::{DataType, PropertyDefinitionBuilder, SchemaBuilder};
+    use sawtooth_sdk::processor::handler::{ContextError, TransactionContext};
+
+    #[derive(Default)]
+    /// A MockTransactionContext that can be used to test GridSchemaState
+    struct MockTransactionContext {
+        state: RefCell<HashMap<String, Vec<u8>>>,
+    }
+
+    impl TransactionContext for MockTransactionContext {
+        fn get_state_entries(
+            &self,
+            addresses: &[String],
+        ) -> Result<Vec<(String, Vec<u8>)>, ContextError> {
+            let mut results = Vec::new();
+            for addr in addresses {
+                let data = match self.state.borrow().get(addr) {
+                    Some(data) => data.clone(),
+                    None => Vec::new(),
+                };
+                results.push((addr.to_string(), data));
+            }
+            Ok(results)
+        }
+
+        fn set_state_entries(&self, entries: Vec<(String, Vec<u8>)>) -> Result<(), ContextError> {
+            for (addr, data) in entries {
+                self.state.borrow_mut().insert(addr, data);
+            }
+            Ok(())
+        }
+
+        /// this is not needed for these tests
+        fn delete_state_entries(&self, _addresses: &[String]) -> Result<Vec<String>, ContextError> {
+            unimplemented!()
+        }
+
+        /// this is not needed for these tests
+        fn add_receipt_data(&self, _data: &[u8]) -> Result<(), ContextError> {
+            unimplemented!()
+        }
+
+        /// this is not needed for these tests
+        fn add_event(
+            &self,
+            _event_type: String,
+            _attributes: Vec<(String, String)>,
+            _data: &[u8],
+        ) -> Result<(), ContextError> {
+            unimplemented!()
+        }
+    }
+
+    #[test]
+    // Test that if an agent does not exist in state, None is returned
+    fn test_get_agent_none() {
+        let mut transaction_context = MockTransactionContext::default();
+        let mut state = GridSchemaState::new(&mut transaction_context);
+
+        let result = state.get_agent("agent_public_key").unwrap();
+        assert!(result.is_none())
+    }
+
+    #[test]
+    // Test that if an agent does exists in state, Some(Agent) is returned;
+    fn test_get_agent_some() {
+        let mut transaction_context = MockTransactionContext::default();
+
+        let builder = AgentBuilder::new();
+        let agent = builder
+            .with_org_id("organization".to_string())
+            .with_public_key("agent_public_key".to_string())
+            .with_active(true)
+            .with_roles(vec!["Role".to_string()])
+            .build()
+            .unwrap();
+
+        let builder = AgentListBuilder::new();
+        let agent_list = builder.with_agents(vec![agent.clone()]).build().unwrap();
+        let agent_bytes = agent_list.into_bytes().unwrap();
+        let agent_address = compute_agent_address("agent_public_key");
+        transaction_context
+            .set_state_entry(agent_address, agent_bytes)
+            .unwrap();
+
+        let mut state = GridSchemaState::new(&mut transaction_context);
+
+        let result = state.get_agent("agent_public_key").unwrap();
+        assert!(result.is_some());
+        let agent = result.unwrap();
+
+        assert_eq!(agent.public_key(), "agent_public_key");
+    }
+
+    #[test]
+    // Test that if an org does not exist in state, None is returned
+    fn test_get_organization_none() {
+        let mut transaction_context = MockTransactionContext::default();
+        let mut state = GridSchemaState::new(&mut transaction_context);
+
+        let result = state.get_organization("test_org").unwrap();
+        assert!(result.is_none())
+    }
+
+    #[test]
+    // Test that if an org does exists in state, Some(Organization) is returned
+    fn test_get_organization_some() {
+        let mut transaction_context = MockTransactionContext::default();
+
+        let builder = OrganizationBuilder::new();
+        let organization = builder
+            .with_org_id("test_org".to_string())
+            .with_name("name".to_string())
+            .with_address("address".to_string())
+            .build()
+            .unwrap();
+
+        let builder = OrganizationListBuilder::new();
+        let organization_list = builder
+            .with_organizations(vec![organization.clone()])
+            .build()
+            .unwrap();
+        let org_bytes = organization_list.into_bytes().unwrap();
+        let org_address = compute_org_address("test_org");
+        transaction_context
+            .set_state_entry(org_address, org_bytes)
+            .unwrap();
+
+        let mut state = GridSchemaState::new(&mut transaction_context);
+
+        let result = state.get_organization("test_org").unwrap();
+        assert!(result.is_some());
+        let organization = result.unwrap();
+
+        assert_eq!(organization.org_id(), "test_org");
+    }
+
+    #[test]
+    // 1. Test that if a schema is not in state a None is returned.
+    // 2. Test that a schema can be added to state using set_state.
+    // 3. Test that if a schema is in state it will be returned as Some(Schema).
+    // 4. Test that a schema can be replaced
+    fn test_grid_schema_state() {
+        let mut transaction_context = MockTransactionContext::default();
+        let mut state = GridSchemaState::new(&mut transaction_context);
+
+        let result = state.get_schema("TestSchema").unwrap();
+        assert!(result.is_none());
+
+        let builder = PropertyDefinitionBuilder::new();
+        let property_definition = builder
+            .with_name("TEST".to_string())
+            .with_data_type(DataType::Enum)
+            .with_description("Optional".to_string())
+            .with_enum_options(vec![
+                "One".to_string(),
+                "Two".to_string(),
+                "Three".to_string(),
+            ])
+            .build()
+            .unwrap();
+
+        let builder = SchemaBuilder::new();
+        let schema = builder
+            .with_name("TestSchema".to_string())
+            .with_description("Test Schema".to_string())
+            .with_owner("owner".to_string())
+            .with_properties(vec![property_definition.clone()])
+            .build()
+            .unwrap();
+
+        assert!(state.set_schema("TestSchema", schema).is_ok());
+        let schema_result = state.get_schema("TestSchema").unwrap();
+        assert!(schema_result.is_some());
+        let schema = schema_result.unwrap();
+        assert_eq!(schema.description(), "Test Schema");
+
+        let builder = SchemaBuilder::new();
+        let schema = builder
+            .with_name("TestSchema".to_string())
+            .with_description("New Description".to_string())
+            .with_owner("owner".to_string())
+            .with_properties(vec![property_definition.clone()])
+            .build()
+            .unwrap();
+
+        assert!(state.set_schema("TestSchema", schema).is_ok());
+        let schema_result = state.get_schema("TestSchema").unwrap();
+        assert!(schema_result.is_some());
+        let schema = schema_result.unwrap();
+        assert_eq!(schema.description(), "New Description");
+    }
+
+}
