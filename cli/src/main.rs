53a54a7cdb08f81981cd0e870a812a22cd036c2d
Ryan Banks <rbanks@bitwise.io>
2019-04-18 15:33:18 -0500

    Add pike commands to CLI
    Signed-off-by: Ryan Banks <rbanks@bitwise.io>

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -23,9 +23,19 @@ mod http;
 mod key;
 mod transaction;
 
+use clap::ArgMatches;
+use grid_sdk::protocol::pike::{
+    payload::{
+        CreateAgentActionBuilder, CreateOrganizationActionBuilder, UpdateAgentActionBuilder,
+        UpdateOrganizationActionBuilder,
+    },
+    state::{KeyValueEntry, KeyValueEntryBuilder},
+};
 use simple_logger;
 
-use error::CliError;
+use crate::error::CliError;
+
+use actions::{agents, organizations as orgs};
 
 const APP_NAME: &str = env!("CARGO_PKG_NAME");
 const VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -36,7 +46,52 @@ fn run() -> Result<(), CliError> {
         (version: VERSION)
         (author: "Contributors to Hyperledger Grid")
         (about: "Command line for Hyperledger Grid")
+        (@arg url: --url  +takes_value "URL for the REST API")
+        (@arg wait: --wait +takes_value "How long to wait for transaction to be committed")
+        (@arg key: -k +takes_value "base name for private key file")
         (@arg verbose: -v +multiple "Log verbosely")
+        (@subcommand agent =>
+            (about: "Update or create agent")
+            (@setting SubcommandRequiredElseHelp)
+            (@subcommand create =>
+                (about: "Create an agent")
+                (@arg org_id: +takes_value +required "organization ID")
+                (@arg public_key: +takes_value +required "public key")
+                (@arg active: "Is user active")
+                (@arg roles: --roles +takes_value +multiple "Roles assigned to agent")
+                (@arg metadata: --metadata +takes_value +multiple
+                    "Comma-separated key value pairs stored in metadata")
+            )
+            (@subcommand update =>
+                (about: "Update an agent")
+                (@arg org_id: +takes_value +required "organization ID")
+                (@arg public_key: +takes_value +required "public key")
+                (@arg active: "Is user active")
+                (@arg roles: --roles +takes_value +multiple "Roles assigned to agent")
+                (@arg metadata: --metadata +takes_value +multiple
+                    "Comma-separated key value pairs stored in metadata")
+            )
+        )
+        (@subcommand organization =>
+            (about: "Update or create organization")
+            (@setting SubcommandRequiredElseHelp)
+            (@subcommand create =>
+                (about: "Create an organization")
+                (@arg org_id: +required +takes_value "Unique ID for organization")
+                (@arg name: +required +takes_value "Name of the organization")
+                (@arg address: +takes_value "Physical address for organization")
+                (@arg metadata: --metadata +takes_value +multiple
+                    "Comma-separated key value pairs stored in metadata")
+            )
+            (@subcommand update =>
+                (about: "Update an organization")
+                (@arg org_id: +required +takes_value "Unique ID for organization")
+                (@arg name: +required +takes_value "Name of the organization")
+                (@arg address: +takes_value "Physical address for organization")
+                (@arg metadata: --metadata +takes_value +multiple
+                    "Comma-separated key value pairs stored in metadata")
+            )
+        )
     )
     .get_matches();
 
@@ -46,7 +101,109 @@ fn run() -> Result<(), CliError> {
         _ => simple_logger::init_with_level(log::Level::Debug),
     }?;
 
+    let url = matches.value_of("url").unwrap_or("http://localhost:8000");
+
+    let key = matches.value_of("key").map(|k| k.to_string());
+
+    let wait = value_t!(matches, "wait", u64).unwrap_or(0);
+
+    match matches.subcommand() {
+        ("agent", Some(m)) => match m.subcommand() {
+            ("create", Some(m)) => {
+                let create_agent = CreateAgentActionBuilder::new()
+                    .with_org_id(m.value_of("org_id").unwrap().into())
+                    .with_public_key(m.value_of("public_key").unwrap().into())
+                    .with_active(m.is_present("active"))
+                    .with_roles(
+                        m.values_of("roles")
+                            .unwrap_or_default()
+                            .map(String::from)
+                            .collect::<Vec<String>>(),
+                    )
+                    .with_metadata(parse_metadata(&m)?)
+                    .build()
+                    .map_err(|err| CliError::UserError(format!("{}", err)))?;
+
+                agents::do_create_agent(&url, key, wait, create_agent)?
+            }
+            ("update", Some(m)) => {
+                let update_agent = UpdateAgentActionBuilder::new()
+                    .with_org_id(m.value_of("org_id").unwrap().into())
+                    .with_public_key(m.value_of("public_key").unwrap().into())
+                    .with_active(m.is_present("active"))
+                    .with_roles(
+                        m.values_of("roles")
+                            .unwrap_or_default()
+                            .map(String::from)
+                            .collect::<Vec<String>>(),
+                    )
+                    .with_metadata(parse_metadata(&m)?)
+                    .build()
+                    .map_err(|err| CliError::UserError(format!("{}", err)))?;
+
+                agents::do_update_agent(&url, key, wait, update_agent)?
+            }
+            _ => return Err(CliError::UserError("Subcommand not recognized".into())),
+        },
+        ("organization", Some(m)) => match m.subcommand() {
+            ("create", Some(m)) => {
+                let create_org = CreateOrganizationActionBuilder::new()
+                    .with_org_id(m.value_of("org_id").unwrap().into())
+                    .with_name(m.value_of("name").unwrap().into())
+                    .with_address(m.value_of("address").unwrap().into())
+                    .with_metadata(parse_metadata(&m)?)
+                    .build()
+                    .map_err(|err| CliError::UserError(format!("{}", err)))?;
+
+                orgs::do_create_organization(&url, key, wait, create_org)?
+            }
+            ("update", Some(m)) => {
+                let update_org = UpdateOrganizationActionBuilder::new()
+                    .with_org_id(m.value_of("org_id").unwrap().into())
+                    .with_name(m.value_of("name").unwrap().into())
+                    .with_address(m.value_of("address").unwrap().into())
+                    .with_metadata(parse_metadata(&m)?)
+                    .build()
+                    .map_err(|err| CliError::UserError(format!("{}", err)))?;
+
+                orgs::do_update_organization(&url, key, wait, update_org)?
+            }
+            _ => return Err(CliError::UserError("Subcommand not recognized".into())),
+        },
+        _ => return Err(CliError::UserError("Subcommand not recognized".into())),
+    }
+
     Ok(())
+}
+
+fn parse_metadata(matches: &ArgMatches) -> Result<Vec<KeyValueEntry>, CliError> {
+    let metadata = matches
+        .values_of("metadata")
+        .unwrap_or_default()
+        .map(String::from)
+        .collect::<Vec<String>>();
+
+    let mut key_value_entries = Vec::new();
+
+    for data in metadata {
+        let entries = data.split('=').map(String::from).collect::<Vec<String>>();
+
+        let (key, value) = if entries.len() != 2 {
+            return Err(CliError::UserError(format!("Metadata malformed: {}", data)));
+        } else {
+            (entries[0].clone(), entries[1].clone())
+        };
+
+        key_value_entries.push(
+            KeyValueEntryBuilder::new()
+                .with_key(key)
+                .with_value(value)
+                .build()
+                .map_err(|err| CliError::UserError(format!("Metadata malformed: {}", err)))?,
+        );
+    }
+
+    Ok(key_value_entries)
 }
 
 fn main() {
