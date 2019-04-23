ea0cceab9137b31239fa54cc853d0b3856d2dc58
Eloá Franca Verona <eloafran@bitwise.io>
2019-04-23 14:26:46 -0500

    Add BatchBuilder for schema
    Adds a new method that returns a batch builder to help build
    transactions and batches for schema transactions.
    
    Signed-off-by: Eloá Franca Verona <eloafran@bitwise.io>

--- cli/src/transaction.rs
+++ cli/src/transaction.rs
@@ -38,6 +38,14 @@ pub const PIKE_NAMESPACE: &str = "cad11d";
 const PIKE_FAMILY_NAME: &str = "pike";
 const PIKE_FAMILY_VERSION: &str = "0.1";
 
+pub const GRID_SCHEMA_NAMESPACE: &str = "621dee01";
+const GRID_SCHEMA_FAMILY_NAME: &str = "grid_schema";
+const GRID_SCHEMA_FAMILY_VERSION: &str = "1.0";
+
+pub fn schema_batch_builder(key: Option<String>) -> BatchBuilder {
+    BatchBuilder::new(GRID_SCHEMA_FAMILY_NAME, GRID_SCHEMA_FAMILY_VERSION, key)
+}
+
 pub fn pike_batch_builder(key: Option<String>) -> BatchBuilder {
     BatchBuilder::new(PIKE_FAMILY_NAME, PIKE_FAMILY_VERSION, key)
 }
