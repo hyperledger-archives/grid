1965a7ae15d416dc5fc6930555976df71c3e66e5
Eloá Franca Verona <eloafran@bitwise.io>
2019-04-23 14:24:45 -0500

    Add tests for parse_yaml and helper methods
    Adds tests for methods responible to parse yaml files with schema
    definition into SchemaPayload structs.
    
    Signed-off-by: Eloá Franca Verona <eloafran@bitwise.io>

--- cli/src/actions/schemas.rs
+++ cli/src/actions/schemas.rs
@@ -287,3 +287,493 @@ fn parse_value_as_vec_string(
         None => Ok(None),
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use grid_sdk::protocol::schema::payload::{Action, SchemaPayload};
+    use grid_sdk::protocol::schema::state::{
+        DataType, PropertyDefinition, PropertyDefinitionBuilder,
+    };
+    use serde_yaml::{Mapping, Value};
+    use std::env;
+    use std::fs::{remove_file, File};
+    use std::io::Write;
+    use std::panic;
+    use std::thread;
+
+    static LIGHTBULB_YAML_EXAMPLE: &[u8; 800] = br##"- name: "Lightbulb"
+  description: "Example Lightbulb schema"
+  properties:
+    - name: "size"
+      data_type: NUMBER
+      description: "Lightbulb radius, in millimeters"
+      number_exponent: 0
+      required: true
+    - name: "bulb_type"
+      data_type: ENUM
+      description: "Bulb type"
+      enum_options: ["filament", "CF", "LED"]
+      required: true
+    - name: "energy_rating"
+      data_type: NUMBER
+      description: "EnergyStar energy rating"
+      number_exponent: 0
+    - name: "color"
+      data_type: STRUCT
+      description: "A named RGB Color value"
+      struct_properties:
+            - name: 'name'
+              data_type: STRING
+              description: "Color name"
+            - name: 'rgb_hex'
+              data_type: STRING
+              description: "RGB value" "##;
+
+    static PHONE_YAML_EXAMPLE: &[u8; 492] = br##"
+- name: "Phone"
+  description: "Example phone schema"
+  properties:
+      - name: "brand"
+        data_type: STRING
+        description: "Name of the brand"
+        required: true
+      - name: "operation_system"
+        data_type: ENUM
+        description: "Phone's operation system"
+        enum_options: ["Android", "iOS"]
+        required: true
+      - name: "price"
+        data_type: NUMBER
+        description: "Price of phone rounded to the nearest dollar"
+        number_exponent: 0"##;
+
+    ///
+    /// Verifies parse_yaml returns a valid SchemaPayload with SchemaCreateAction set from a yaml
+    /// containing a single schema definition
+    ///
+    #[test]
+    fn test_valid_yaml_create_one_schema() {
+        run_test(|test_yaml_file_path| {
+            let mut file =
+                File::create(test_yaml_file_path).expect("Error creating test schema yaml file.");
+
+            file.write_all(LIGHTBULB_YAML_EXAMPLE)
+                .expect("Error writting example schema.");
+
+            let payload =
+                parse_yaml(test_yaml_file_path, Action::SchemaCreate).expect("Error parsing yaml");
+
+            assert_eq!(make_create_schema_payload_1(), payload[0]);
+        })
+    }
+
+    ///
+    /// Verifies parse_yaml returns valids SchemaPayload with SchemaCreateAction set from a yaml
+    /// containing a multiple schema definitions
+    ///
+    #[test]
+    fn test_valid_yaml_create_multiple_schemas() {
+        run_test(|test_yaml_file_path| {
+            let mut file =
+                File::create(test_yaml_file_path).expect("Error creating test schema yaml file.");
+            file.write(LIGHTBULB_YAML_EXAMPLE)
+                .expect("Error writting example schema.");
+            file.write(PHONE_YAML_EXAMPLE)
+                .expect("Error writting example schema.");
+
+            let payload =
+                parse_yaml(test_yaml_file_path, Action::SchemaCreate).expect("Error parsing yaml");
+
+            assert_eq!(make_create_schema_payload_1(), payload[0]);
+            assert_eq!(make_create_schema_payload_2(), payload[1]);
+        })
+    }
+
+    ///
+    /// Verifies parse_yaml returns a valid SchemaPayload with SchemaUpdateAction set from a yaml
+    /// containing a single schema definition
+    ///
+    #[test]
+    fn test_valid_yaml_update_one_schema() {
+        run_test(|test_yaml_file_path| {
+            let mut file =
+                File::create(test_yaml_file_path).expect("Error creating test schema yaml file.");
+
+            file.write_all(LIGHTBULB_YAML_EXAMPLE)
+                .expect("Error writting example schema.");
+
+            let payload =
+                parse_yaml(test_yaml_file_path, Action::SchemaUpdate).expect("Error parsing yaml");
+
+            assert_eq!(make_update_schema_payload_1(), payload[0]);
+        })
+    }
+
+    ///
+    /// Verifies parse_yaml returns valids SchemaPayload with SchemaUpdateAction set from a yaml
+    /// containing a multiple schema definitions
+    ///
+    #[test]
+    fn test_valid_yaml_update_multiple_schemas() {
+        run_test(|test_yaml_file_path| {
+            let mut file =
+                File::create(test_yaml_file_path).expect("Error creating test schema yaml file.");
+            file.write(LIGHTBULB_YAML_EXAMPLE)
+                .expect("Error writting example schema.");
+            file.write(PHONE_YAML_EXAMPLE)
+                .expect("Error writting example schema.");
+
+            let payload =
+                parse_yaml(test_yaml_file_path, Action::SchemaUpdate).expect("Error parsing yaml");
+
+            assert_eq!(make_update_schema_payload_1(), payload[0]);
+            assert_eq!(make_update_schema_payload_2(), payload[1]);
+        })
+    }
+
+    ///
+    /// Verifies parse_data_type returns the expected data_types for valid inputs and returns an
+    /// error for a invalid input
+    ///
+    #[test]
+    fn test_parse_data_type() {
+        // Check the method returns the expected data types for each valid input
+        assert_eq!(parse_data_type("string").unwrap(), DataType::String);
+        assert_eq!(parse_data_type("bytes").unwrap(), DataType::Bytes);
+        assert_eq!(parse_data_type("NUMBER").unwrap(), DataType::Number);
+        assert_eq!(parse_data_type("enum").unwrap(), DataType::Enum);
+        assert_eq!(parse_data_type("Struct").unwrap(), DataType::Struct);
+
+        // Check the method returns an error for an invalid input
+        assert!(parse_data_type("not_a_valid_type").is_err());
+    }
+
+    ///
+    /// Verifies parse_value_as_sequence can parse Values as Sequence for valid inputs
+    ///  and returns an error for invalid inputs
+    ///
+    #[test]
+    fn test_parse_value_as_sequence() {
+        let key = "sequence".to_string();
+        let mut property_valid = Mapping::new();
+
+        // Check method can properly parse a sequence value
+        let sequence = Vec::<Value>::new();
+        property_valid.insert(
+            Value::String(key.clone()),
+            Value::Sequence(sequence.clone()),
+        );
+        assert_eq!(
+            parse_value_as_sequence(&property_valid, &key).unwrap(),
+            Some(sequence)
+        );
+
+        // Check method returns an error when key is found but value cannot be parsed into a
+        // sequence
+        let mut property_invalid = Mapping::new();
+        property_invalid.insert(
+            Value::String(key.clone()),
+            Value::String("not a sequence".to_string()),
+        );
+        assert!(parse_value_as_sequence(&property_invalid, &key).is_err());
+
+        // Check method returns Ok(None) when key is not found
+        assert!(parse_value_as_sequence(&Mapping::new(), &key)
+            .unwrap()
+            .is_none());
+    }
+
+    ///
+    /// Verifies parse_value_as_string can parse Values as String for valid inputs
+    ///  and returns an error for invalid inputs
+    ///
+    #[test]
+    fn test_parse_value_as_string() {
+        let key = "string".to_string();
+        let string_value = "my string".to_string();
+
+        // Check method can properly parse a string value
+        let mut property_valid = Mapping::new();
+        property_valid.insert(
+            Value::String(key.clone()),
+            Value::String(string_value.clone()),
+        );
+        assert_eq!(
+            parse_value_as_string(&property_valid, &key).unwrap(),
+            Some(string_value.clone())
+        );
+
+        // Check method returns an error when key is found but value cannot be parsed into a
+        // string
+        let mut property_invalid = Mapping::new();
+        property_invalid.insert(Value::String(key.clone()), Value::Number(0.into()));
+        assert!(parse_value_as_string(&property_invalid, &key).is_err());
+
+        // Check method returns Ok(None) when key is not found
+        assert!(parse_value_as_string(&Mapping::new(), &key)
+            .unwrap()
+            .is_none());
+    }
+
+    ///
+    /// Verifies parse_value_as_boolean can parse Values as booleans for valid inputs
+    ///  and returns an error for invalid inputs
+    ///
+    #[test]
+    fn test_parse_value_as_boolean() {
+        let key = "bool".to_string();
+        let bool_value = true;
+
+        // Check method can properly parse a boolean value
+        let mut property_valid = Mapping::new();
+        property_valid.insert(Value::String(key.clone()), Value::Bool(bool_value.clone()));
+        assert_eq!(
+            parse_value_as_boolean(&property_valid, &key).unwrap(),
+            Some(bool_value.clone())
+        );
+
+        // Check method returns an error when key is found but value cannot be parsed into a
+        // boolean
+        let mut property_invalid = Mapping::new();
+        property_invalid.insert(Value::String(key.clone()), Value::Number(0.into()));
+        assert!(parse_value_as_boolean(&property_invalid, &key).is_err());
+
+        // Check method returns Ok(None) when key is not found
+        assert!(parse_value_as_boolean(&Mapping::new(), &key)
+            .unwrap()
+            .is_none());
+    }
+
+    ///
+    /// Verifies parse_parse_value_as_i32 can parse Values as i32 for valid inputs
+    ///  and returns an error for invalid inputs
+    ///
+    #[test]
+    fn test_parse_value_as_i32() {
+        let key = "number_i32".to_string();
+        let number_value = 200;
+
+        // Check method can properly parse a number value
+        let mut property_valid = Mapping::new();
+        property_valid.insert(
+            Value::String(key.clone()),
+            Value::Number(number_value.clone().into()),
+        );
+        assert_eq!(
+            parse_value_as_i32(&property_valid, &key).unwrap(),
+            Some(number_value.clone())
+        );
+
+        // Check method returns an error when key is found but value overflows an i32 capacity.
+        let mut property_invalid = Mapping::new();
+        property_invalid.insert(
+            Value::String(key.clone()),
+            Value::Number(3000000000_i64.into()),
+        );
+        assert!(parse_value_as_i32(&property_invalid, &key).is_err());
+
+        // Check method returns an error when key is found but value cannot be parsed into a
+        // number
+        let mut property_invalid = Mapping::new();
+        property_invalid.insert(
+            Value::String(key.clone()),
+            Value::String("Not a number".to_string()),
+        );
+        assert!(parse_value_as_i32(&property_invalid, &key).is_err());
+
+        // Check method returns Ok(None) when key is not found
+        assert!(parse_value_as_i32(&Mapping::new(), &key).unwrap().is_none());
+    }
+
+    ///
+    /// Verifies pparse_value_as_vec_string can parse Values as Vec<String> for valid inputs
+    ///  and returns an error for invalid inputs
+    ///
+    #[test]
+    fn test_parse_value_as_vec_string() {
+        let key = "vec".to_string();
+        let string_value = "value".to_string();
+        let sequence_value = vec![Value::String(string_value.clone())];
+
+        // Check method can properly parse a Value into Vec<String>
+        let mut property_valid = Mapping::new();
+        property_valid.insert(Value::String(key.clone()), Value::Sequence(sequence_value));
+        assert_eq!(
+            parse_value_as_vec_string(&property_valid, &key).unwrap(),
+            Some(vec![string_value.clone()])
+        );
+
+        // Check method returns an error when key is found but values inside the sequence cannot
+        // be parsed into a string.
+        let mut property_invalid = Mapping::new();
+        let sequence_value_invalid = vec![Value::Number(0.into())];
+        property_invalid.insert(
+            Value::String(key.clone()),
+            Value::Sequence(sequence_value_invalid),
+        );
+        assert!(parse_value_as_vec_string(&property_invalid, &key).is_err());
+
+        // Check method returns an error when key is found but value cannot be parsed into a vec
+        let mut property_invalid = Mapping::new();
+        property_invalid.insert(
+            Value::String(key.clone()),
+            Value::String("Not a sequence".to_string()),
+        );
+        assert!(parse_value_as_vec_string(&property_invalid, &key).is_err());
+
+        // Check method returns Ok(None) when key is not found
+        assert!(parse_value_as_i32(&Mapping::new(), &key).unwrap().is_none());
+    }
+
+    fn make_create_schema_payload_1() -> SchemaPayload {
+        generate_create_schema_payload(
+            "Lightbulb",
+            &create_lightbulb_property_definitions(),
+            Some("Example Lightbulb schema".to_string()),
+        )
+        .unwrap()
+    }
+
+    fn make_create_schema_payload_2() -> SchemaPayload {
+        generate_create_schema_payload(
+            "Phone",
+            &create_phone_property_definitions(),
+            Some("Example phone schema".to_string()),
+        )
+        .unwrap()
+    }
+
+    fn make_update_schema_payload_1() -> SchemaPayload {
+        generate_update_schema_payload("Lightbulb", &create_lightbulb_property_definitions())
+            .unwrap()
+    }
+
+    fn make_update_schema_payload_2() -> SchemaPayload {
+        generate_update_schema_payload("Phone", &create_phone_property_definitions()).unwrap()
+    }
+
+    fn create_lightbulb_property_definitions() -> Vec<PropertyDefinition> {
+        vec![
+            make_number_property_definition("size", 0, true, "Lightbulb radius, in millimeters"),
+            make_enum_property_definition(
+                "bulb_type",
+                &["filament".to_string(), "CF".to_string(), "LED".to_string()],
+                true,
+                "Bulb type",
+            ),
+            make_number_property_definition("energy_rating", 0, false, "EnergyStar energy rating"),
+            make_struct_property_definition(
+                "color",
+                &[
+                    make_string_property_definition("name", false, "Color name"),
+                    make_string_property_definition("rgb_hex", false, "RGB value"),
+                ],
+                false,
+                "A named RGB Color value",
+            ),
+        ]
+    }
+
+    fn create_phone_property_definitions() -> Vec<PropertyDefinition> {
+        vec![
+            make_string_property_definition("brand", true, "Name of the brand"),
+            make_enum_property_definition(
+                "operation_system",
+                &["Android".to_string(), "iOS".to_string()],
+                true,
+                "Phone's operation system",
+            ),
+            make_number_property_definition(
+                "price",
+                0,
+                false,
+                "Price of phone rounded to the nearest dollar",
+            ),
+        ]
+    }
+
+    fn make_enum_property_definition(
+        name: &str,
+        options: &[String],
+        required: bool,
+        description: &str,
+    ) -> PropertyDefinition {
+        PropertyDefinitionBuilder::new()
+            .with_name(name.to_string())
+            .with_data_type(DataType::Enum)
+            .with_enum_options(options.to_vec())
+            .with_required(required)
+            .with_description(description.to_string())
+            .build()
+            .unwrap()
+    }
+
+    fn make_number_property_definition(
+        name: &str,
+        number_exponent: i32,
+        required: bool,
+        description: &str,
+    ) -> PropertyDefinition {
+        PropertyDefinitionBuilder::new()
+            .with_name(name.to_string())
+            .with_data_type(DataType::Number)
+            .with_number_exponent(number_exponent)
+            .with_required(required)
+            .with_description(description.to_string())
+            .build()
+            .unwrap()
+    }
+
+    fn make_struct_property_definition(
+        name: &str,
+        properties: &[PropertyDefinition],
+        required: bool,
+        description: &str,
+    ) -> PropertyDefinition {
+        PropertyDefinitionBuilder::new()
+            .with_name(name.to_string())
+            .with_data_type(DataType::Struct)
+            .with_struct_properties(properties.to_vec())
+            .with_required(required)
+            .with_description(description.to_string())
+            .build()
+            .unwrap()
+    }
+
+    fn make_string_property_definition(
+        name: &str,
+        required: bool,
+        description: &str,
+    ) -> PropertyDefinition {
+        PropertyDefinitionBuilder::new()
+            .with_name(name.to_string())
+            .with_data_type(DataType::String)
+            .with_required(required)
+            .with_description(description.to_string())
+            .build()
+            .unwrap()
+    }
+
+    fn run_test<T>(test: T) -> ()
+    where
+        T: FnOnce(&str) -> () + panic::UnwindSafe,
+    {
+        let test_yaml_file = temp_yaml_file_path();
+
+        let test_path = test_yaml_file.clone();
+        let result = panic::catch_unwind(move || test(&test_path));
+
+        remove_file(test_yaml_file).unwrap();
+
+        assert!(result.is_ok())
+    }
+
+    fn temp_yaml_file_path() -> String {
+        let mut temp_dir = env::temp_dir();
+
+        let thread_id = thread::current().id();
+        temp_dir.push(format!("test_parse_schema-{:?}.yaml", thread_id));
+        temp_dir.to_str().unwrap().to_string()
+    }
+}
