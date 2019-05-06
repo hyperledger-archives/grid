df2ab5ef9083fbb02ac5229c6db0cb9b980d460f
Eloá Franca Verona <eloafran@bitwise.io>
2019-05-06 10:39:48 -0500

    Add LatLongValue to diesel schema and models
    Signed-off-by: Eloá Franca Verona <eloafran@bitwise.io>

--- daemon/src/database/models.rs
+++ daemon/src/database/models.rs
@@ -15,7 +15,13 @@
  * -----------------------------------------------------------------------------
  */
 
+use byteorder::{NetworkEndian, ReadBytesExt};
+use diesel::deserialize::{self, FromSql};
+use diesel::pg::Pg;
+use diesel::serialize::{self, Output, ToSql, WriteTuple};
+use diesel::sql_types;
 use serde_json::Value as JsonValue;
+use std::io::Write;
 
 use super::schema::{
     agent, block, grid_property_definition, grid_property_value, grid_schema, organization,
@@ -148,6 +154,7 @@ pub struct NewGridPropertyValue {
     pub string_value: Option<String>,
     pub enum_value: Option<i32>,
     pub struct_values: Option<Vec<String>>,
+    pub lat_long_value: Option<LatLongValue>,
 }
 
 #[derive(Queryable, Debug)]
@@ -163,4 +170,51 @@ pub struct GridPropertyValue {
     pub string_value: Option<String>,
     pub enum_value: Option<i64>,
     pub struct_values: Option<Vec<String>>,
+    pub lat_long_value: Option<LatLongValue>,
+}
+
+#[derive(SqlType, QueryId, Debug, Clone, Copy)]
+#[postgres(type_name = "latlong")]
+pub struct LatLong;
+
+#[derive(Debug, PartialEq, FromSqlRow, AsExpression)]
+#[sql_type = "LatLong"]
+pub struct LatLongValue(pub i64, pub i64);
+
+impl ToSql<LatLong, Pg> for LatLongValue {
+    fn to_sql<W: Write>(&self, out: &mut Output<W, Pg>) -> serialize::Result {
+        WriteTuple::<(sql_types::BigInt, sql_types::BigInt)>::write_tuple(&(self.0, self.1), out)
+    }
+}
+
+impl FromSql<LatLong, Pg> for LatLongValue {
+    fn from_sql(bytes: Option<&[u8]>) -> deserialize::Result<Self> {
+        let mut bytes = not_none!(bytes);
+        let num_elements = bytes.read_i32::<NetworkEndian>()?;
+        if num_elements != 2 {
+            return Err(format!("Expected a tuple of 2 elements, got {}", num_elements,).into());
+        }
+        let (_, mut bytes) = bytes.split_at(std::mem::size_of::<i32>() * 2);
+        let lat = bytes.read_i64::<NetworkEndian>()?;
+        let (_, mut bytes) = bytes.split_at(std::mem::size_of::<i32>() * 2);
+        let long = bytes.read_i64::<NetworkEndian>()?;
+        Ok(LatLongValue(lat, long))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_deserializing_lat_long_value_from_bytes() {
+        let lat_long_bytes = [
+            0, 0, 0, 2, 0, 0, 0, 20, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 20, 0, 0, 0, 8,
+            255, 255, 255, 255, 255, 255, 255, 255,
+        ];
+        let lat_long = LatLongValue::from_sql(Some(&lat_long_bytes))
+            .expect("Failed to deserialize LatLongValue");
+
+        assert_eq!(lat_long, LatLongValue(0, -1));
+    }
 }
