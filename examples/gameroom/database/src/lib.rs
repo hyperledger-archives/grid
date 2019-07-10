9d27215ee056c3092f7c77b1947f340c3e303e07
Shannyn Telander <telander@bitwise.io>
2019-07-10 12:33:32 -0500

    Add database Connection Pool
    Adds ConnectionPool for the database instance.
    
    Signed-off-by: Shannyn Telander <telander@bitwise.io>

--- examples/gameroom/database/src/lib.rs
+++ examples/gameroom/database/src/lib.rs
@@ -17,4 +17,44 @@
 
 pub mod error;
 
+use std::ops::Deref;
+
+use diesel::{
+    pg::PgConnection,
+    r2d2::{ConnectionManager, Pool, PooledConnection},
+};
+
 pub use crate::error::DatabaseError;
+
+pub fn create_connection_pool(database_url: &str) -> Result<ConnectionPool, DatabaseError> {
+    let connection_manager = ConnectionManager::<PgConnection>::new(database_url);
+    Ok(ConnectionPool {
+        pool: Pool::builder()
+            .build(connection_manager)
+            .map_err(|err| DatabaseError::ConnectionError(Box::new(err)))?,
+    })
+}
+
+pub struct Connection(PooledConnection<ConnectionManager<PgConnection>>);
+
+impl Deref for Connection {
+    type Target = PgConnection;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+#[derive(Clone)]
+pub struct ConnectionPool {
+    pool: Pool<ConnectionManager<PgConnection>>,
+}
+
+impl ConnectionPool {
+    pub fn get(&self) -> Result<Connection, DatabaseError> {
+        self.pool
+            .get()
+            .map(Connection)
+            .map_err(|err| DatabaseError::ConnectionError(Box::new(err)))
+    }
+}
