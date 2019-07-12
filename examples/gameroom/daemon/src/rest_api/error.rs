212b81efa25249af4df51d0c6d7a89616e0c8346
Shannyn Telander <telander@bitwise.io>
2019-07-12 13:32:27 -0500

    Add initial Gameroom Daemon errors
    Adds an initial logging error for the Gameroom Daemon as well
    as an initial std error for the rest API.
    
    Signed-off-by: Shannyn Telander <telander@bitwise.io>

---
+++ examples/gameroom/daemon/src/rest_api/error.rs
@@ -0,0 +1,43 @@
+// Copyright 2019 Cargill Incorporated
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::error::Error;
+use std::fmt;
+
+#[derive(Debug)]
+pub enum RestApiServerError {
+    StdError(std::io::Error),
+}
+
+impl Error for RestApiServerError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            RestApiServerError::StdError(err) => Some(err),
+        }
+    }
+}
+
+impl fmt::Display for RestApiServerError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            RestApiServerError::StdError(e) => write!(f, "Std Error: {}", e),
+        }
+    }
+}
+
+impl From<std::io::Error> for RestApiServerError {
+    fn from(err: std::io::Error) -> RestApiServerError {
+        RestApiServerError::StdError(err)
+    }
+}
