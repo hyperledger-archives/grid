14f330187dd4c655bc705afbaa3530f573b539a3
Shannyn Telander <telander@bitwise.io>
2019-07-24 15:40:05 -0500

    Initialize logging and start up
    Adds an initial start up method for the Gameroom Daemon
    an initializes logging.
    
    Signed-off-by: Shannyn Telander <telander@bitwise.io>

--- examples/gameroom/daemon/src/main.rs
+++ examples/gameroom/daemon/src/main.rs
@@ -12,7 +12,42 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[macro_use]
+extern crate clap;
+#[macro_use]
+extern crate log;
+
 mod error;
 mod rest_api;
 
-pub fn main() {}
+use crate::error::GameroomDaemonError;
+use simple_logger;
+
+const APP_NAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+fn run() -> Result<(), GameroomDaemonError> {
+    let matches = clap_app!(myapp =>
+        (name: APP_NAME)
+        (version: VERSION)
+        (author: "Cargill Incorporated")
+        (about: "Daemon Package for Gameroom")
+        (@arg verbose: -v +multiple "Log verbosely")
+    )
+    .get_matches();
+
+    match matches.occurrences_of("verbose") {
+        0 => simple_logger::init_with_level(log::Level::Warn),
+        1 => simple_logger::init_with_level(log::Level::Info),
+        _ => simple_logger::init_with_level(log::Level::Debug),
+    }?;
+
+    Ok(())
+}
+
+fn main() {
+    if let Err(e) = run() {
+        error!("{}", e);
+        std::process::exit(1);
+    }
+}
