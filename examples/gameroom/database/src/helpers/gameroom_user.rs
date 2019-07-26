11884e5405b2de9a430e2470901c179f7ff25533
Shannyn Telander <telander@bitwise.io>
2019-07-26 13:50:28 -0500

    Add method to fetch user by email
    Adds a method as a database helper to fetch a Gameroom user
    by the specified email.
    
    Signed-off-by: Shannyn Telander <telander@bitwise.io>

---
+++ examples/gameroom/database/src/helpers/gameroom_user.rs
@@ -0,0 +1,26 @@
+// Copyright 2019 Cargill Incorporated
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::models::GameroomUser;
+use crate::schema::gameroom_user;
+
+use diesel::{pg::PgConnection, prelude::*, result::Error::NotFound, QueryResult};
+
+pub fn fetch_user_by_email(conn: &PgConnection, email: &str) -> QueryResult<Option<GameroomUser>> {
+    gameroom_user::table
+        .filter(gameroom_user::email.eq(email))
+        .first::<GameroomUser>(conn)
+        .map(Some)
+        .or_else(|err| if err == NotFound { Ok(None) } else { Err(err) })
+}
