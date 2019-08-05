c3b6ce05d0a18de2e36c9e37667e55762ec2a99b
Eloá Franca Verona <eloafran@bitwise.io>
2019-08-05 12:56:22 -0500

    Add circuit_proposal and related tables
    This commit adds to the gameroom database tables to store information on
    circuit proposals and votes on proposals.
    
    Signed-off-by: Eloá Franca Verona <eloafran@bitwise.io>

--- examples/gameroom/database/tables/gameroom_tables.sql
+++ examples/gameroom/database/tables/gameroom_tables.sql
@@ -19,3 +19,45 @@ CREATE TABLE IF NOT EXISTS gameroom_user (
   email                     TEXT        NOT NULL,
   hashed_password           TEXT        NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS circuit_proposal (
+  id                        TEXT        PRIMARY KEY,
+  proposal_type             TEXT        NOT NULL ,
+  circuit_id                TEXT        NOT NULL,
+  circuit_hash              TEXT        NOT NULL,
+  requester                 TEXT        NOT NULL,
+  authorization_type        TEXT        NOT NULL,
+  persistence               TEXT        NOT NULL,
+  routes                    TEXT        NOT NULL,
+  circuit_management_type   TEXT        NOT NULL,
+  application_metadata      BYTEA       NOT NULL,
+  status                    TEXT        NOT NULL,
+  created_time              TIMESTAMP   NOT NULL,
+  updated_time              TIMESTAMP   NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS proposal_vote_record(
+  id                        BIGSERIAL   PRIMARY KEY,
+  proposal_id               TEXT        NOT NULL,
+  voter_public_key          TEXT        NOT NULL,
+  vote                      TEXT        NOT NULL,
+  created_time              TIMESTAMP   NOT NULL,
+  FOREIGN KEY (proposal_id) REFERENCES circuit_proposal(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS proposal_circuit_member (
+  id                        BIGSERIAL   PRIMARY KEY,
+  proposal_id               TEXT        NOT NULL,
+  node_id                   TEXT        NOT NULL,
+  endpoint                  TEXT        NOT NULL,
+  FOREIGN KEY (proposal_id) REFERENCES circuit_proposal(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS proposal_circuit_service (
+  id                        BIGSERIAL   PRIMARY KEY,
+  proposal_id               TEXT        NOT NULL,
+  service_id                TEXT        NOT NULL,
+  service_type              TEXT        NOT NULL,
+  allowed_nodes             TEXT[][]    NOT NULL,
+  FOREIGN KEY (proposal_id) REFERENCES circuit_proposal(id) ON DELETE CASCADE
+);
