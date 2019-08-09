a18c81657a0c3db0217d792ab776ccffc62a5c6e
Andrea Gunderson <agunde@bitwise.io>
2019-08-09 13:10:36 -0500

    Add Two Phase Commit consensus to AdminService
    The AdminService now starts up a TwoPhase consensus engine.
    The engine will validate that the required nodes in a
    circuit proposal all have the circuit proposal after
    recieving the CircuitManagmentPayload. The proposal managers use
    the hash of the paylaod as its id and verifies that the
    services are working off the same hash of a circuit proposal.
    The ProposalManager now contains the code for sending the
    circuit payload on to the other splinter nodes in the
    circuit proposal.
    
    This commit also changes create_circuit to use the
    AdminServiceState instead of a clone of the AdminService
    because the admin service needs to hold on to a join handle
    for the consensus engine, which is not cloneable.
    This also requires that the AdminServiceState now holds the
    implementation for propose_circuit.
    
    Signed-off-by: Andrea Gunderson <agunde@bitwise.io>

--- libsplinter/src/admin/state.rs
+++ libsplinter/src/admin/state.rs
@@ -13,41 +13,219 @@
 // limitations under the License.
 
 use std::collections::HashMap;
+use std::collections::VecDeque;
 
 use crossbeam_channel::Sender;
 
-use crate::admin::messages::AdminServiceEvent;
+use crate::admin::error::AdminStateError;
+use crate::admin::messages;
+use crate::admin::sha256;
+use crate::consensus::{Proposal, ProposalId};
 use crate::network::peer::PeerConnector;
-use crate::protos::admin::CircuitProposal;
+use crate::protos::admin::{
+    Circuit, CircuitCreateRequest, CircuitManagementPayload, CircuitManagementPayload_Action,
+    CircuitProposal, CircuitProposal_ProposalType,
+};
+use crate::service::error::ServiceError;
 use crate::service::ServiceNetworkSender;
 
 pub struct AdminServiceState {
-    pub open_proposals: HashMap<String, CircuitProposal>,
-    pub peer_connector: PeerConnector,
-    pub network_sender: Option<Box<dyn ServiceNetworkSender>>,
-    pub socket_senders: Vec<(String, Sender<AdminServiceEvent>)>,
+    node_id: String,
+    open_proposals: HashMap<String, CircuitProposal>,
+    peer_connector: PeerConnector,
+    network_sender: Option<Box<dyn ServiceNetworkSender>>,
+    pending_circuit_payloads: VecDeque<CircuitManagementPayload>,
+    pending_consesus_proposals: HashMap<ProposalId, (Proposal, CircuitManagementPayload)>,
+    pending_changes: Option<CircuitProposal>,
+    socket_senders: Vec<(String, Sender<messages::AdminServiceEvent>)>,
 }
 
 impl AdminServiceState {
-    pub fn add_proposal(&mut self, circuit_proposal: CircuitProposal) {
-        let circuit_id = circuit_proposal.get_circuit_id().to_string();
+    pub fn new(node_id: String, peer_connector: PeerConnector) -> Self {
+        AdminServiceState {
+            node_id: node_id.to_string(),
+            network_sender: None,
+            open_proposals: Default::default(),
+            peer_connector,
+            pending_circuit_payloads: VecDeque::new(),
+            pending_consesus_proposals: HashMap::new(),
+            pending_changes: None,
+            socket_senders: Vec::new(),
+        }
+    }
 
-        self.open_proposals.insert(circuit_id, circuit_proposal);
+    pub fn node_id(&self) -> &str {
+        &self.node_id
+    }
+
+    pub fn network_sender(&self) -> &Option<Box<dyn ServiceNetworkSender>> {
+        &self.network_sender
+    }
+
+    pub fn set_network_sender(&mut self, network_sender: Option<Box<dyn ServiceNetworkSender>>) {
+        self.network_sender = network_sender;
+    }
+
+    pub fn pop_pending_circuit_payload(&mut self) -> Option<CircuitManagementPayload> {
+        self.pending_circuit_payloads.pop_front()
+    }
+
+    pub fn pending_consesus_proposals(
+        &self,
+        id: &ProposalId,
+    ) -> Option<&(Proposal, CircuitManagementPayload)> {
+        self.pending_consesus_proposals.get(id)
+    }
+
+    pub fn remove_pending_consesus_proposals(
+        &mut self,
+        id: &ProposalId,
+    ) -> Option<(Proposal, CircuitManagementPayload)> {
+        self.pending_consesus_proposals.remove(id)
+    }
+
+    pub fn add_pending_consesus_proposal(
+        &mut self,
+        id: ProposalId,
+        proposal: (Proposal, CircuitManagementPayload),
+    ) {
+        self.pending_consesus_proposals.insert(id, proposal);
+    }
+
+    pub fn pending_changes(&self) -> &Option<CircuitProposal> {
+        &self.pending_changes
+    }
+
+    pub fn commit(&mut self) -> Result<(), AdminStateError> {
+        match self.pending_changes.take() {
+            Some(circuit_proposal) => {
+                let circuit_id = circuit_proposal.get_circuit_id().to_string();
+                let mgmt_type = circuit_proposal
+                    .get_circuit_proposal()
+                    .circuit_management_type
+                    .clone();
+
+                self.add_proposal(circuit_proposal.clone());
+
+                // notify registered authorization application handlers of the commited circuit
+                // proposal
+                let event = messages::AdminServiceEvent::ProposalSubmitted(
+                    messages::CircuitProposal::from_proto(circuit_proposal.clone()).map_err(
+                        |err| AdminStateError(format!("invalid message format {}", err)),
+                    )?,
+                );
+                self.send_event(&mgmt_type, event);
+
+                info!("committed change for circuit proposal {}", circuit_id,);
+
+                Ok(())
+            }
+            None => Err(AdminStateError("no pending changes to commit".into())),
+        }
+    }
+
+    pub fn rollback(&mut self) -> Result<(), AdminStateError> {
+        match self.pending_changes.take() {
+            Some(circuit_proposal) => {
+                info!("discarded change for {}", circuit_proposal.get_circuit_id())
+            }
+            None => debug!("no changes to rollback"),
+        }
+
+        Ok(())
+    }
+
+    pub fn propose_change(
+        &mut self,
+        mut circuit_payload: CircuitManagementPayload,
+    ) -> Result<(String, CircuitProposal), AdminStateError> {
+        match circuit_payload.get_action() {
+            CircuitManagementPayload_Action::CIRCUIT_CREATE_REQUEST => {
+                let mut create_request = circuit_payload.take_circuit_create_request();
+                let proposed_circuit = create_request.take_circuit();
+
+                if self.has_proposal(proposed_circuit.get_circuit_id()) {
+                    Err(AdminStateError(format!(
+                        "Ignoring duplicate create proposal of circuit {}",
+                        proposed_circuit.get_circuit_id()
+                    )))
+                } else {
+                    debug!("proposing {}", proposed_circuit.get_circuit_id());
+
+                    let mut circuit_proposal = CircuitProposal::new();
+                    circuit_proposal.set_proposal_type(CircuitProposal_ProposalType::CREATE);
+                    circuit_proposal.set_circuit_id(proposed_circuit.get_circuit_id().into());
+                    circuit_proposal.set_circuit_hash(sha256(&proposed_circuit)?);
+                    circuit_proposal.set_circuit_proposal(proposed_circuit);
+
+                    let expected_hash = sha256(&circuit_proposal)?;
+                    self.pending_changes = Some(circuit_proposal.clone());
+
+                    Ok((expected_hash, circuit_proposal))
+                }
+            }
+            unknown_action => Err(AdminStateError(format!(
+                "Unable to handle {:?}",
+                unknown_action
+            ))),
+        }
     }
 
     pub fn has_proposal(&self, circuit_id: &str) -> bool {
         self.open_proposals.contains_key(circuit_id)
     }
 
+    /// Propose a new circuit
+    ///
+    /// This operation will propose a new circuit to all the member nodes of the circuit.  If there
+    /// is no peer connection, a connection to the peer will also be established.
+    pub fn propose_circuit(&mut self, proposed_circuit: Circuit) -> Result<(), ServiceError> {
+        debug!("Adding members as peers");
+
+        for node in proposed_circuit.get_members() {
+            if self.node_id() != node.get_node_id() {
+                debug!("Connecting to node {:?}", node);
+                self.peer_connector
+                    .connect_peer(node.get_node_id(), node.get_endpoint())
+                    .map_err(|err| ServiceError::UnableToHandleMessage(Box::new(err)))?;
+            }
+        }
+        debug!("Members added");
+
+        debug!(
+            "recieved circuit proposal for {}",
+            proposed_circuit.get_circuit_id()
+        );
+        let mut create_request = CircuitCreateRequest::new();
+        create_request.set_circuit(proposed_circuit);
+
+        let mut envelope = CircuitManagementPayload::new();
+        envelope.set_action(CircuitManagementPayload_Action::CIRCUIT_CREATE_REQUEST);
+        envelope.set_circuit_create_request(create_request);
+
+        self.pending_circuit_payloads.push_back(envelope);
+        Ok(())
+    }
+
+    fn add_proposal(&mut self, circuit_proposal: CircuitProposal) {
+        let circuit_id = circuit_proposal.get_circuit_id().to_string();
+
+        self.open_proposals.insert(circuit_id, circuit_proposal);
+    }
+
     pub fn add_socket_sender(
         &mut self,
         circuit_management_type: String,
-        sender: Sender<AdminServiceEvent>,
+        sender: Sender<messages::AdminServiceEvent>,
     ) {
         self.socket_senders.push((circuit_management_type, sender));
     }
 
-    pub fn send_event(&mut self, circuit_management_type: &str, event: AdminServiceEvent) {
+    pub fn send_event(
+        &mut self,
+        circuit_management_type: &str,
+        event: messages::AdminServiceEvent,
+    ) {
         // The use of retain allows us to drop any senders that are no longer valid.
         self.socket_senders.retain(|(mgmt_type, sender)| {
             if mgmt_type != circuit_management_type {
