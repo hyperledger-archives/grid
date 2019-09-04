d8be77a6652875da1959e5012ddb8cc7e2ae34d4
Darian Plumb <dplumb@bitwise.io>
2019-09-04 13:49:09 -0500

    Move invitation cards to component
      - Adds a new InvitationCard component
      - Moves the cards out of the Invitations view and into their own
        component
      - Instruments the action buttons
    
    Signed-off-by: Darian Plumb <dplumb@bitwise.io>

---
+++ examples/gameroom/gameroom-app/src/components/InvitationCard.vue
@@ -0,0 +1,141 @@
+<!--
+Copyright 2019 Cargill Incorporated
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-->
+
+<template>
+  <div class="card-container">
+    <div class="header">
+      <div class="title">
+        Name placeholder
+      </div>
+    </div>
+    <div class="body">
+      <div class="data">
+        <div class="key">{{ getTimestampLabel(proposal.requester) }}</div>
+        <div class="value">
+          {{ fromNow(proposal.created_time) }}
+        </div>
+      </div>
+      <div class="data">
+        <div class="key">from:</div>
+        <div class="value">
+          From placeholder
+        </div>
+      </div>
+      <div class="data">
+        <div class="key">members:</div>
+        <div class="value">
+          <li class="list-value"
+              v-for="(member, index) in proposal.members"
+              :key="index">
+            {{ member.node_id }}
+          </li>
+        </div>
+      </div>
+    </div>
+    <div v-if="!isSelf(proposal.requester)" class="actions">
+      <button :disabled="!canSubmit"
+              class="btn-action table"
+              @click="acceptInvitation">
+        <div v-if="acceptSubmitting" class="spinner" />
+        <div v-else class="btn-text">Accept</div>
+      </button>
+      <button :disabled="!canSubmit"
+              class="btn-action table outline"
+              @click="rejectInvitation">
+        <div v-if="rejectSubmitting" class="spinner" />
+        <div class="btn-text">Reject</div>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Prop, Component } from 'vue-property-decorator';
+import * as moment from 'moment';
+import { GameroomProposal } from '../store/models';
+import proposals from '@/store/modules/proposals';
+
+@Component
+export default class InvitationCard extends Vue {
+  @Prop() proposal!: GameroomProposal;
+
+  acceptSubmitting = false;
+  rejectSubmitting = false;
+
+  get canSubmit() {
+    return !this.acceptSubmitting && !this.rejectSubmitting;
+  }
+
+  fromNow(timestamp: number): string {
+    return moment.unix(timestamp).fromNow();
+  }
+
+  isSelf(key: string): boolean {
+    const publicKey = this.$store.getters['user/getPublicKey'];
+    return (key === publicKey);
+  }
+
+  getTimestampLabel(key: string) {
+    if (this.isSelf(key)) {
+      return 'sent:';
+    } else {
+      return 'received:';
+    }
+  }
+
+  async acceptInvitation() {
+    this.acceptSubmitting = true;
+    try {
+      await proposals.vote({
+        proposalID: this.proposal.proposal_id,
+        ballot: {
+          circuit_id: this.proposal.circuit_id,
+          circuit_hash: this.proposal.circuit_hash,
+          vote: 'Accept',
+        },
+      });
+      this.$emit('success', 'You have accepted the invitation.');
+    } catch (e) {
+      console.error(e);
+      this.$emit('error', e.message);
+    }
+    this.acceptSubmitting = false;
+  }
+
+  async rejectInvitation() {
+    this.rejectSubmitting = true;
+    try {
+      await proposals.vote({
+        proposalID: this.proposal.proposal_id,
+        ballot: {
+          circuit_id: this.proposal.circuit_id,
+          circuit_hash: this.proposal.circuit_hash,
+          vote: 'Reject',
+        },
+      });
+      this.$emit('success', 'You have rejected the invitation.');
+    } catch (e) {
+      console.error(e);
+      this.$emit('error', e.message);
+    }
+    this.rejectSubmitting = false;
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '@/scss/components/_invitation-card.scss';
+</style>
--- examples/gameroom/gameroom-app/src/views/Invitations.vue
+++ examples/gameroom/gameroom-app/src/views/Invitations.vue
@@ -18,79 +18,34 @@ limitations under the License.
   <div class="invitations-container">
     <h2 class="title">Invitations</h2>
     <div v-if="proposals.length > 0" class="data-container">
-      <div class="card-container" v-for="(proposal, index) in proposals" :key="index">
-        <div class="header">
-          <div class="title">
-            <h2>{{ proposal.proposal_id }}</h2>
-          </div>
-        </div>
-        <div class="body">
-          <div class="data">
-            <div class="key">{{ getTimestampLabel(proposal.requester) }}</div>
-            <div class="value">
-              {{ fromNow(proposal.created_time) }}
-            </div>
-          </div>
-          <div class="data">
-            <div class="key">from:</div>
-            <div class="value breakable-value">
-              {{ proposal.requester }}
-            </div>
-          </div>
-          <div class="data">
-            <div class="key">members:</div>
-            <div class="value">
-              <li class="list-value" v-for="(member, index) in proposal.members" :key="index">
-                {{ member.node_id }}
-              </li>
-            </div>
-          </div>
-        </div>
-        <div v-if="!isSelf(proposal.requester)" class="actions">
-          <button class="btn-action table">
-            <div class="btn-text">Accept</div>
-          </button>
-          <button class="btn-action table outline">
-            <div class="btn-text">Reject</div>
-          </button>
-        </div>
-      </div>
+      <invitation-card
+        v-on:success="$emit('success', $event)"
+        v-on:error="$emit('error', $event)"
+        class="card-container"
+        v-for="(proposal, index) in proposals"
+        :key="index"
+        :proposal="proposal" />
     </div>
-    <h3 v-else class="tbl-placeholder" >No pending proposals</h3>
+    <h3 v-else class="tbl-placeholder">No pending proposals</h3>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
+import InvitationCard from '@/components/InvitationCard.vue';
 import proposals from '@/store/modules/proposals';
-import * as moment from 'moment';
 
-@Component
+@Component({
+  components: { InvitationCard },
+})
 export default class Invitations extends Vue {
 
   mounted() {
-    proposals.listProposalsMock();
+    proposals.listProposals();
   }
 
   get proposals() {
     return proposals.proposalList;
-  }
-
-  fromNow(timestamp: number): string {
-    return moment.unix(timestamp).fromNow();
-  }
-
-  isSelf(key: string): boolean {
-    const publicKey = this.$store.getters['user/getPublicKey'];
-    return (key === publicKey);
-  }
-
-  getTimestampLabel(key: string) {
-    if (this.isSelf(key)) {
-      return 'sent:';
-    } else {
-      return 'received:';
-    }
   }
 }
 </script>
@@ -106,6 +61,7 @@ export default class Invitations extends Vue {
       padding: 1rem;
       margin-top: 2rem;
       overflow: auto;
+      align-items: center;
     }
   }
 </style>
