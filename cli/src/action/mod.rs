56589efa15a785bddcb28cb428755897a332d7dc
Ryan Banks <rbanks@bitwise.io>
2019-10-02 14:52:48 -0500

    Add health status subcommand to cli
    Signed-off-by: Ryan Banks <rbanks@bitwise.io>

--- cli/src/action/mod.rs
+++ cli/src/action/mod.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 pub mod admin;
+#[cfg(feature = "health")]
+pub mod health;
 
 use std::collections::HashMap;
 
