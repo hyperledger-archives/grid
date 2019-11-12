b1aa0eb2a3cab91c916bfe643d32ba40745075e0
Shawn T. Amundson <amundson@bitwise.io>
2019-11-12 15:02:28 -0600

    Add "pub use" statements for "client" admin bits
    This re-exports the pieces of the API which are used by Gameroom as an admin
    service client. This is part of a series of commits to separate service,
    REST API, and client components within the admin service code.
    
    Signed-off-by: Shawn T. Amundson <amundson@bitwise.io>

--- libsplinter/src/admin/error.rs
+++ libsplinter/src/admin/error.rs
@@ -12,6 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub mod error;
-pub mod messages;
-pub mod service;
+pub use super::service::error::MarshallingError;
--- libsplinter/src/admin/messages.rs
+++ libsplinter/src/admin/messages.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub mod error;
-pub mod messages;
-pub mod service;
+pub use super::service::messages::{
+    AdminServiceEvent, AuthorizationType, CircuitProposal, CircuitProposalVote, CreateCircuit,
+    DurabilityType, PersistenceType, ProposalType, RouteType, SplinterNode, SplinterService, Vote,
+    VoteRecord,
+};
