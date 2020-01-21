5f9aee44ed9eec531c6ffa5da97a3b7110ea2c9c
Peter Schwarz <pschwarz@bitwise.io>
2020-01-21 16:34:15 -0600

    Add Protocol Version Range Guard
    The ProtocolVersionRangeGuard evaluates the optional header value of
    `SplinterVersion` and, if present, rejects requests if they are not
    within the range specified.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- libsplinter/src/rest_api/mod.rs
+++ libsplinter/src/rest_api/mod.rs
@@ -330,6 +330,94 @@ impl RequestGuard for Box<dyn RequestGuard> {
     }
 }
 
+/// Guards requests based on a minimum protocol version.
+///
+/// A protocol version is specified via the HTTP header `"SplinterProtocolVersion"`.  This header
+/// is a positive integer value.
+#[derive(Clone)]
+pub struct ProtocolVersionRangeGuard {
+    min: u32,
+    max: u32,
+}
+
+impl ProtocolVersionRangeGuard {
+    /// Constructs a new protocol version guard with the given minimum.
+    pub fn new(min: u32, max: u32) -> Self {
+        Self { min, max }
+    }
+}
+
+impl RequestGuard for ProtocolVersionRangeGuard {
+    fn evaluate(&self, req: &HttpRequest) -> Continuation {
+        if let Some(header_value) = req.headers().get("SplinterProtocolVersion") {
+            let parsed_header = header_value
+                .to_str()
+                .map_err(|err| {
+                    format!(
+                        "Invalid characters in SplinterProtocolVersion header: {}",
+                        err
+                    )
+                })
+                .and_then(|val_str| {
+                    val_str.parse::<u32>().map_err(|_| {
+                        "SplinterProtocolVersion must be a valid positive integer".to_string()
+                    })
+                });
+            match parsed_header {
+                Err(msg) => Continuation::terminate(
+                    HttpResponse::BadRequest()
+                        .json(json!({
+                            "message": msg,
+                        }))
+                        .into_future(),
+                ),
+                Ok(version) if version < self.min => Continuation::terminate(
+                    HttpResponse::BadRequest()
+                        .json(json!({
+                            "message": format!(
+                                "Client must support protocol version {} or greater.",
+                                self.min,
+                            ),
+                            "requested_protocol": version,
+                            "splinter_protocol": self.max,
+                            "libsplinter_version": format!(
+                                "{}.{}.{}",
+                                env!("CARGO_PKG_VERSION_MAJOR"),
+                                env!("CARGO_PKG_VERSION_MINOR"),
+                                env!("CARGO_PKG_VERSION_PATCH")
+                            )
+                        }))
+                        .into_future(),
+                ),
+                Ok(version) if version > self.max => Continuation::terminate(
+                    HttpResponse::BadRequest()
+                        .json(json!({
+                            "message": format!(
+                                "Client requires a newer protocol than can be provided: {} > {}",
+                                version,
+                                self.max,
+                            ),
+                            "requested_protocol": version,
+                            "splinter_protocol": self.max,
+                            "libsplinter_version": format!(
+                                "{}.{}.{}",
+                                env!("CARGO_PKG_VERSION_MAJOR"),
+                                env!("CARGO_PKG_VERSION_MINOR"),
+                                env!("CARGO_PKG_VERSION_PATCH")
+                            )
+                        }))
+                        .into_future(),
+                ),
+                Ok(_) => Continuation::Continue,
+            }
+        } else {
+            // Ignore the missing header, and assume the client will handle version mismatches by
+            // inspecting the output
+            Continuation::Continue
+        }
+    }
+}
+
 /// `RestApi` is used to create an instance of a restful web server.
 #[derive(Clone)]
 pub struct RestApi {
