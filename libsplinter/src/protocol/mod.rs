2bb8ff14e2bb5600490af82ab34164f45bf50a28
Peter Schwarz <pschwarz@bitwise.io>
2020-01-22 10:08:15 -0600

    Guard Admin routes with protocol versions
    The admin routes are guarded with individual min versions for each
    endpoint and a single protocol version for the current version.
    
    Each successes group of endpoints should have an overarching current
    version going forward.  For example, scabbard routes might change more
    rapidly than the admin routes.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- libsplinter/src/protocol/mod.rs
+++ libsplinter/src/protocol/mod.rs
@@ -13,3 +13,15 @@
 // limitations under the License.
 
 //! Protocol versions for various endpoints provided by splinter.
+
+pub const ADMIN_PROTOCOL_VERSION: u32 = 1;
+
+pub const ADMIN_APPLICATION_REGISTRATION_PROTOCOL_MIN: u32 = 1;
+
+pub const ADMIN_SUBMIT_PROTOCOL_MIN: u32 = 1;
+
+#[cfg(feature = "proposal-read")]
+pub const ADMIN_FETCH_PROPOSALS_PROTOCOL_MIN: u32 = 1;
+
+#[cfg(feature = "proposal-read")]
+pub const ADMIN_LIST_PROPOSALS_PROTOCOL_MIN: u32 = 1;
