d7242bd6048e1d539fcd3d73729b356d19b32c3e
Logan Seeley <seeley@bitwise.io>
2020-01-31 11:56:57 -0600

    Add state get methods to scabbard client
    Add methods to the scabbard client for getting state by a specific
    address or by address prefix. These methods correspond to the `/state`
    and `/state/{address}` REST API endpoints for scabbard.
    
    Signed-off-by: Logan Seeley <seeley@bitwise.io>

--- libsplinter/src/service/scabbard/client/mod.rs
+++ libsplinter/src/service/scabbard/client/mod.rs
@@ -16,7 +16,12 @@ mod error;
 mod scar;
 mod submit;
 
+use reqwest::{blocking::Client, Url};
 use sawtooth_sdk::messages::batch::BatchList;
+
+use crate::hex::parse_hex;
+
+use super::SERVICE_TYPE;
 
 pub use error::Error;
 pub use scar::{SabreSmartContractDefinition, SabreSmartContractMetadata};
@@ -49,5 +54,109 @@ impl ScabbardClient {
         } else {
             Ok(())
         }
+    }
+
+    pub fn get_state_at_address(
+        &self,
+        circuit_id: &str,
+        service_id: &str,
+        address: &str,
+    ) -> Result<Option<Vec<u8>>, Error> {
+        parse_hex(address).map_err(|err| Error::new_with_source("invalid address", err.into()))?;
+
+        let url = Url::parse(&format!(
+            "{}/{}/{}/{}/state/{}",
+            &self.url, SERVICE_TYPE, circuit_id, service_id, address
+        ))
+        .map_err(|err| Error::new_with_source("invalid URL", err.into()))?;
+
+        let request = Client::new().get(url);
+        let response = request
+            .send()
+            .map_err(|err| Error::new_with_source("request failed", err.into()))?;
+
+        if response.status().is_success() {
+            Ok(Some(response.json().map_err(|err| {
+                Error::new_with_source("failed to deserialize response body", err.into())
+            })?))
+        } else if response.status().as_u16() == 404 {
+            Ok(None)
+        } else {
+            let status = response.status();
+            let msg: ErrorResponse = response.json().map_err(|err| {
+                Error::new_with_source("failed to deserialize error response body", err.into())
+            })?;
+            Err(Error::new(&format!(
+                "failed to get state at address: {}: {}",
+                status, msg
+            )))
+        }
+    }
+
+    pub fn get_state_with_prefix(
+        &self,
+        circuit_id: &str,
+        service_id: &str,
+        prefix: Option<&str>,
+    ) -> Result<Vec<StateEntry>, Error> {
+        let mut url = Url::parse(&format!(
+            "{}/{}/{}/{}/state",
+            &self.url, SERVICE_TYPE, circuit_id, service_id
+        ))
+        .map_err(|err| Error::new_with_source("invalid URL", err.into()))?;
+        if let Some(prefix) = prefix {
+            parse_hex(prefix).map_err(|err| Error::new_with_source("invalid prefix", err.into()))?;
+            if prefix.len() > 70 {
+                return Err(Error::new("prefix must be less than 70 characters"));
+            }
+            url.set_query(Some(&format!("prefix={}", prefix)))
+        }
+
+        let request = Client::new().get(url);
+        let response = request
+            .send()
+            .map_err(|err| Error::new_with_source("request failed", err.into()))?;
+
+        if response.status().is_success() {
+            response.json().map_err(|err| {
+                Error::new_with_source("failed to deserialize response body", err.into())
+            })
+        } else {
+            let status = response.status();
+            let msg: ErrorResponse = response.json().map_err(|err| {
+                Error::new_with_source("failed to deserialize error response body", err.into())
+            })?;
+            Err(Error::new(&format!(
+                "failed to get state with prefix: {}: {}",
+                status, msg
+            )))
+        }
+    }
+}
+
+#[derive(Deserialize, Debug)]
+pub struct StateEntry {
+    address: String,
+    value: Vec<u8>,
+}
+
+impl StateEntry {
+    pub fn address(&self) -> &str {
+        &self.address
+    }
+
+    pub fn value(&self) -> &[u8] {
+        &self.value
+    }
+}
+
+#[derive(Deserialize, Debug)]
+struct ErrorResponse {
+    message: String,
+}
+
+impl std::fmt::Display for ErrorResponse {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.message)
     }
 }
