52906ad22f49e676a659b715f15adbda366118a6
Ryan Banks <rbanks@bitwise.io>
2020-02-06 13:31:16 -0600

    Add CircuitStoreError
    Signed-off-by: Ryan Banks <rbanks@bitwise.io>

--- libsplinter/src/circuit/store.rs
+++ libsplinter/src/circuit/store.rs
@@ -21,3 +21,45 @@ pub trait CircuitStore: Send + Sync + Clone {
 
     fn circuit(&self, circuit_name: &str) -> Result<Option<Circuit>, CircuitStoreError>;
 }
+
+#[derive(Debug)]
+pub struct CircuitStoreError {
+    context: String,
+    source: Option<Box<dyn std::error::Error + Send + 'static>>,
+}
+
+impl std::error::Error for CircuitStoreError {}
+
+impl CircuitStoreError {
+    pub fn new(context: String) -> Self {
+        Self {
+            context,
+            source: None,
+        }
+    }
+
+    pub fn from_source<T: std::error::Error + Send + 'static>(context: String, source: T) -> Self {
+        Self {
+            context,
+            source: Some(Box::new(source)),
+        }
+    }
+
+    pub fn context(&self) -> String {
+        self.context.clone()
+    }
+}
+
+impl std::fmt::Display for CircuitStoreError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        if let Some(ref source) = self.source {
+            write!(
+                f,
+                "CircuitStoreError: Source: {} Context: {}",
+                source, self.context
+            )
+        } else {
+            write!(f, "CircuitStoreError: Context {}", self.context)
+        }
+    }
+}
