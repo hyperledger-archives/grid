e3340b5aed7c6812e7b836f95c4f67bdfd634dd7
Shawn T. Amundson <amundson@bitwise.io>
2020-02-09 14:00:56 -0600

    Add admin::rest_api::actix module
    This module will contain the actix implementation for the admin
    service's REST API.
    
    Signed-off-by: Shawn T. Amundson <amundson@bitwise.io>

--- libsplinter/src/admin/rest_api/mod.rs
+++ libsplinter/src/admin/rest_api/mod.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[cfg(feature = "rest-api-actix")]
+mod actix;
 #[cfg(feature = "proposal-read")]
 mod error;
 #[cfg(feature = "proposal-read")]
