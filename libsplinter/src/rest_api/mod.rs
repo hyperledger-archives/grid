416d159ac9d343b3937323cae6e17e03924e135e
Peter Schwarz <pschwarz@bitwise.io>
2020-02-14 08:58:32 -0600

    Implement display for Method
    Implement display for HTTP Method, which will display the method in the
    standard format of all-caps.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- libsplinter/src/rest_api/mod.rs
+++ libsplinter/src/rest_api/mod.rs
@@ -161,6 +161,19 @@ pub enum Method {
     Head,
 }
 
+impl std::fmt::Display for Method {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Method::Get => f.write_str("GET"),
+            Method::Post => f.write_str("POST"),
+            Method::Put => f.write_str("PUT"),
+            Method::Patch => f.write_str("PATCH"),
+            Method::Delete => f.write_str("DELETE"),
+            Method::Head => f.write_str("HEAD"),
+        }
+    }
+}
+
 /// `Resource` represents a RESTful endpoint.
 ///
 /// ```
