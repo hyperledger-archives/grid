23724f640a0b3c1d4bd2893d8c2516cdec87dd91
Davey Newhall <newhall@bitwise.io>
2020-03-19 10:44:42 -0500

    Add Chips components
    Adds the Chips and Chip components for displaying lists of items. The
    Chip component can also accept additional data to display on hover.
    The hover styles should be defined from the parent component.
    
    Signed-off-by: Davey Newhall <newhall@bitwise.io>

---
+++ grid-ui/saplings/product/src/components/Chips.js
@@ -0,0 +1,61 @@
+/**
+ * Copyright 2018-2020 Cargill Incorporated
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import './Chips.scss';
+
+export function Chips({ children }) {
+  return <div className="chips">{children}</div>;
+}
+
+export function Chip({ label, removeFn, data, deleteable }) {
+  return (
+    <div className="chip-group">
+      <div className="chip">
+        <span className="label">{label}</span>
+        {deleteable && (
+          <FontAwesomeIcon icon="times" className="delete" onClick={removeFn} />
+        )}
+      </div>
+      <div className="chip-data">{data}</div>
+    </div>
+  );
+}
+
+Chips.propTypes = {
+  children: PropTypes.node
+};
+
+Chips.defaultProps = {
+  children: undefined
+};
+
+Chip.propTypes = {
+  label: PropTypes.string,
+  removeFn: PropTypes.func,
+  data: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  deleteable: PropTypes.bool
+};
+
+Chip.defaultProps = {
+  label: '',
+  removeFn: undefined,
+  data: undefined,
+  deleteable: false
+};
