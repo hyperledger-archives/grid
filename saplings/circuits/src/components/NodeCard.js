2bf22559e2a493eb0b9c1d717ef1459b7eec1063
Eloá Franca Verona <eloafran@bitwise.io>
2020-06-22 12:08:28 -0500

    Add node import statement to NodeCard
    Signed-off-by: Eloá Franca Verona <eloafran@bitwise.io>

--- saplings/circuits/src/components/NodeCard.js
+++ saplings/circuits/src/components/NodeCard.js
@@ -17,6 +17,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Node } from '../data/nodeRegistry';
 
 import './NodeCard.scss';
 
