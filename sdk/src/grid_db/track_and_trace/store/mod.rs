71242992dcbc270a13ebbea6df9caa74b239d221
Ryan Banks <rbanks@bitwise.io>
2020-10-08 10:56:24 -0500

    Have Tnt LatLong derive default
    Dabase handler uses default to create reported value objects. This is to
    preserve previous functionality
    
    Signed-off-by: Ryan Banks <rbanks@bitwise.io>

--- sdk/src/grid_db/track_and_trace/store/mod.rs
+++ sdk/src/grid_db/track_and_trace/store/mod.rs
@@ -131,7 +131,7 @@ pub struct ReportedValueReporterToAgentMetadata {
 #[derive(Debug, Clone, Copy, Serialize)]
 pub struct LatLong;
 
-#[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
+#[derive(Default, Debug, PartialEq, Clone, Serialize, Deserialize)]
 pub struct LatLongValue(pub i64, pub i64);
 
 pub trait TrackAndTraceStore: Send + Sync {
