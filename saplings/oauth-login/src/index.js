a6ba55b9afc92669babfc439ec81b0f6467f1232
Shannyn Telander <telander@bitwise.io>
2020-10-22 11:16:17 -0500

    Add initial OAuth login sapling
    Adds the basic boilerplate code to support the OAuth login sapling.
    Also dds the necessary additions to the `sapling-dev-server` folder and
    Dockerfiles to load the OAuth login sapling alongside the other
    saplings.
    
    Signed-off-by: Shannyn Telander <telander@bitwise.io>

---
+++ saplings/oauth-login/src/index.js
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2018-2020 Cargill Incorporated
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  registerConfigSapling,
+  registerApp,
+  hideCanopy
+} from 'splinter-saplingjs';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+registerConfigSapling('oauth-login', () => {
+  if (window.location.pathname === '/oauth-login') {
+    hideCanopy();
+    registerApp(domNode => {
+      ReactDOM.render(<App />, domNode);
+    });
+  }
+});
