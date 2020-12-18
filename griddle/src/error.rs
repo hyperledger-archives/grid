712efcc34f75cc1a1b99cc5a458f249eff9f7302
Ryan Banks <rbanks@bitwise.io>
2020-12-18 10:35:58 -0600

    Add griddle project
    Griddle is grid's integration component. It is responible for providing
    a high level interface for interacting with a grid daemon. It current
    has on endpoint `/submit` that consumes a JSON payload describing a list
    of batches, converts those batches into bytes, and signs them before
    sending them to the grid daemon.
    
    Usage:
    
    griddle 0.1.0
    Contributors to Hyperledger Grid
    Grid Integration Component
    
    USAGE:
        griddle [FLAGS] [OPTIONS]
    
    FLAGS:
        -h, --help       Prints help information
        -q, --quiet      Do not display output
        -V, --version    Prints version information
        -v               Log verbosely
    
    OPTIONS:
        -b, --bind <bind>            Connection endpoint for REST API
            --grid-url <grid_url>    grid daemon that griddle sends requests to
        -k, --key <key>              Base name for private signing key file
    
    Starting griddle:
    
    ./griddle --bind localhost:8080 -k root --grid-url http://grid-instance.com
    
    Submitting payloads to griddle:
    
    {
        "circuit_id": "xyz",
        "service:": "1234",
        "batches": [{"transactions": [{
            "family_name": "pike",
            "version": "0.1",
            "dependencies": [],
            "inputs": ["cad11d"],
            "outputs": ["cad11d"],
            "payload": {
                "action": "CreateOrganization",
                "create_organization": {
                    "name": "Cargill",
                    "org_id": "cgl",
                    "address": "612 worf ave",
                    "metadata": [{
                        "key": "prefix",
                        "value": "12345"
                    }]
                }
            }
    
        }]}]
    }
    
    Signed-off-by: Ryan Banks <rbanks@bitwise.io>

---
+++ griddle/src/error.rs
@@ -0,0 +1,41 @@
+// Copyright 2018-2020 Cargill Incorporated
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::error;
+use std::fmt;
+
+#[derive(Debug)]
+pub struct Error {
+    message: String,
+}
+
+impl Error {
+    pub fn from_message(message: &str) -> Self {
+        Self {
+            message: message.to_string(),
+        }
+    }
+}
+
+impl error::Error for Error {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        None
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
