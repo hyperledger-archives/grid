046ed0cd7ed447a9ee056f05f2d0f8ab5809fc38
Ryan Banks <rbanks@bitwise.io>
2021-03-04 13:48:46 -0600

    Add pike, product, schema, submitter, and location to state
    This is to have feature parody with the state object in the grid daemon.
    
    Signed-off-by: Ryan Banks <rbanks@bitwise.io>

--- sdk/src/rest_api/actix_web_3/mod.rs
+++ sdk/src/rest_api/actix_web_3/mod.rs
@@ -16,10 +16,22 @@ pub mod routes;
 
 use std::sync::Arc;
 
-use routes::submit;
-
+#[cfg(feature = "batch-store")]
 use crate::batches::{store::diesel::DieselBatchStore, BatchStore};
 use crate::error::InternalError;
+#[cfg(feature = "location")]
+use crate::locations::{store::diesel::DieselLocationStore, LocationStore};
+#[cfg(feature = "pike")]
+use crate::pike::{store::diesel::DieselPikeStore, PikeStore};
+#[cfg(feature = "product")]
+use crate::products::{store::diesel::DieselProductStore, ProductStore};
+use crate::rest_api::resources::error::ErrorResponse;
+#[cfg(feature = "schema")]
+use crate::schemas::{store::diesel::DieselSchemaStore, SchemaStore};
+#[cfg(feature = "batch-submitter")]
+use crate::submitter::BatchSubmitter;
+#[cfg(feature = "track-and-trace")]
+use crate::track_and_trace::{store::diesel::DieselTrackAndTraceStore, TrackAndTraceStore};
 
 use actix_web::{App, HttpServer};
 use diesel::r2d2::{ConnectionManager, Pool};
@@ -27,7 +39,20 @@ use diesel::r2d2::{ConnectionManager, Pool};
 #[derive(Clone)]
 pub struct State {
     pub key_file_name: String,
+    #[cfg(feature = "batch-submitter")]
+    pub batch_submitter: Option<Arc<dyn BatchSubmitter + 'static>>,
+    #[cfg(feature = "batch-store")]
     pub batch_store: Arc<dyn BatchStore>,
+    #[cfg(feature = "location")]
+    pub location_store: Arc<dyn LocationStore>,
+    #[cfg(feature = "pike")]
+    pub pike_store: Arc<dyn PikeStore>,
+    #[cfg(feature = "product")]
+    pub product_store: Arc<dyn ProductStore>,
+    #[cfg(feature = "schema")]
+    pub schema_store: Arc<dyn SchemaStore>,
+    #[cfg(feature = "track-and-trace")]
+    pub tnt_store: Arc<dyn TrackAndTraceStore>,
 }
 
 impl State {
@@ -35,11 +60,35 @@ impl State {
         key_file_name: &str,
         connection_pool: Pool<ConnectionManager<diesel::pg::PgConnection>>,
     ) -> Self {
-        let batch_store = Arc::new(DieselBatchStore::new(connection_pool));
+        #[cfg(feature = "batch-store")]
+        let batch_store = Arc::new(DieselBatchStore::new(connection_pool.clone()));
+        #[cfg(feature = "location")]
+        let location_store = Arc::new(DieselLocationStore::new(connection_pool.clone()));
+        #[cfg(feature = "pike")]
+        let pike_store = Arc::new(DieselPikeStore::new(connection_pool.clone()));
+        #[cfg(feature = "product")]
+        let product_store = Arc::new(DieselProductStore::new(connection_pool.clone()));
+        #[cfg(feature = "schema")]
+        let schema_store = Arc::new(DieselSchemaStore::new(connection_pool.clone()));
+        #[cfg(feature = "track-and-trace")]
+        let tnt_store = Arc::new(DieselTrackAndTraceStore::new(connection_pool));
 
         Self {
             key_file_name: key_file_name.to_string(),
+            #[cfg(feature = "batch-submitter")]
+            batch_submitter: None,
+            #[cfg(feature = "batch-store")]
             batch_store,
+            #[cfg(feature = "location")]
+            location_store,
+            #[cfg(feature = "pike")]
+            pike_store,
+            #[cfg(feature = "product")]
+            product_store,
+            #[cfg(feature = "schema")]
+            schema_store,
+            #[cfg(feature = "track-and-trace")]
+            tnt_store,
         }
     }
 
@@ -47,11 +96,43 @@ impl State {
         key_file_name: &str,
         connection_pool: Pool<ConnectionManager<diesel::sqlite::SqliteConnection>>,
     ) -> Self {
-        let batch_store = Arc::new(DieselBatchStore::new(connection_pool));
+        #[cfg(feature = "batch-store")]
+        let batch_store = Arc::new(DieselBatchStore::new(connection_pool.clone()));
+        #[cfg(feature = "location")]
+        let location_store = Arc::new(DieselLocationStore::new(connection_pool.clone()));
+        #[cfg(feature = "pike")]
+        let pike_store = Arc::new(DieselPikeStore::new(connection_pool.clone()));
+        #[cfg(feature = "product")]
+        let product_store = Arc::new(DieselProductStore::new(connection_pool.clone()));
+        #[cfg(feature = "schema")]
+        let schema_store = Arc::new(DieselSchemaStore::new(connection_pool.clone()));
+        #[cfg(feature = "track-and-trace")]
+        let tnt_store = Arc::new(DieselTrackAndTraceStore::new(connection_pool));
 
         Self {
             key_file_name: key_file_name.to_string(),
+            #[cfg(feature = "batch-submitter")]
+            batch_submitter: None,
+            #[cfg(feature = "batch-store")]
             batch_store,
+            #[cfg(feature = "location")]
+            location_store,
+            #[cfg(feature = "pike")]
+            pike_store,
+            #[cfg(feature = "product")]
+            product_store,
+            #[cfg(feature = "schema")]
+            schema_store,
+            #[cfg(feature = "track-and-trace")]
+            tnt_store,
+        }
+    }
+
+    #[cfg(feature = "batch-submitter")]
+    pub fn set_batch_submitter(&mut self, batch_submitter: Arc<dyn BatchSubmitter + 'static>) {
+        self.batch_submitter = Some(batch_submitter);
+    }
+}
         }
     }
 }
