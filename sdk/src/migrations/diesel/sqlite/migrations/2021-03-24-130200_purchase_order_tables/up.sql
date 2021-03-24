27280b9c9bd6d55244dd53f4e052d4dbf7db8a2e
Davey Newhall <newhall@bitwise.io>
2021-03-24 13:21:02 -0500

    Add initial PO migrations
    This adds the initial postgres and sqlite migrations for the purchase
    order module.
    
    Signed-off-by: Davey Newhall <newhall@bitwise.io>

---
+++ sdk/src/migrations/diesel/sqlite/migrations/2021-03-24-130200_purchase_order_tables/up.sql
@@ -0,0 +1,58 @@
+-- Copyright 2021 Cargill Incorporated
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- -----------------------------------------------------------------------------
+
+CREATE TABLE purchase_order (
+    id INTEGER PRIMARY KEY,
+    uid VARCHAR NOT NULL,
+    workflow_status VARCHAR NOT NULL,
+    is_closed BOOLEAN NOT NULL,
+    accepted_version_id VARCHAR NOT NULL,
+    start_commit_num BIGINT NOT NULL,
+    end_commit_num BIGINT NOT NULL,
+    service_id TEXT
+);
+
+CREATE TABLE purchase_order_version (
+    id INTEGER PRIMARY KEY,
+    purchase_order_uid VARCHAR NOT NULL,
+    version_id VARCHAR NOT NULL,
+    is_draft BOOLEAN NOT NULL,
+    current_revision_number BIGINT NOT NULL,
+    start_commit_num BIGINT NOT NULL,
+    end_commit_num BIGINT NOT NULL,
+    service_id TEXT,
+);
+
+CREATE TABLE purchase_order_version_revision (
+    id INTEGER PRIMARY KEY,
+    version_id VARCHAR NOT NULL,
+    revision_number BIGINT NOT NULL,
+    order_xml_3_4 VARCHAR NOT NULL,
+    submitter VARCHAR NOT NULL,
+    created_at BIGINT NOT NULL,
+    start_commit_num BIGINT NOT NULL,
+    end_commit_num BIGINT NOT NULL,
+    service_id TEXT
+);
+
+CREATE TABLE purchase_order_alternate_id (
+    id INTEGER PRIMARY KEY,
+    purchase_order_uid VARCHAR NOT NULL,
+    alternate_id_type VARCHAR NOT NULL,
+    alternate_id VARCHAR NOT NULL,
+    start_commit_num BIGINT NOT NULL,
+    end_commit_num BIGINT NOT NULL,
+    service_id TEXT
+);
