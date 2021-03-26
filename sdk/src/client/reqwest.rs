6d1a3378ac0713ad1d3c68181dd3120989f80a15
Isabel Tomb <tomb@bitwise.io>
2021-03-26 12:09:32 -0500

    Implement `post_batches`
    Implement the `post_batches` method in the reqwest based `Client`
    implementation.
    
    Signed-off-by: Isabel Tomb <tomb@bitwise.io>

--- sdk/src/client/reqwest.rs
+++ sdk/src/client/reqwest.rs
@@ -14,7 +14,13 @@
 
 //! A Reqwest-based implementation of Client
 
-use crate::batches::store::BatchList;
+use protobuf::Message;
+use reqwest::blocking::Client as BlockingClient;
+use sawtooth_sdk::messages::batch::BatchList;
+use serde::Deserialize;
+use std::collections::HashMap;
+use std::time::Instant;
+
 use crate::error::InternalError;
 
 use super::Client;
@@ -37,8 +43,127 @@ impl ReqwestClient {
 }
 
 impl Client for ReqwestClient {
-    fn post_batches(&self, _batches: BatchList) -> Result<(), InternalError> {
-        unimplemented!()
+    /// Submits a list of batches
+    fn post_batches(
+        &self,
+        batch_list: &BatchList,
+        service_id: Option<&str>,
+        wait: u64,
+    ) -> Result<(), InternalError> {
+        let bytes = batch_list.write_to_bytes().map_err(|err| {
+            InternalError::from_source_with_message(
+                Box::new(err),
+                "Failed to convert batch list to bytes".to_string(),
+            )
+        })?;
+
+        let mut wait_time = wait;
+
+        let mut url = format!("{}/batches", self.url);
+
+        if let Some(service_id) = service_id {
+            url.push_str(&format!("?service_id={}", service_id));
+        }
+
+        let client = BlockingClient::new();
+
+        let response = client
+            .post(&url)
+            .header("GridProtocolVersion", "1")
+            .body(bytes)
+            .send()
+            .map_err(|err| {
+                InternalError::from_source_with_message(
+                    Box::new(err),
+                    "Failed to post batch list".to_string(),
+                )
+            })?;
+
+        if !response.status().is_success() {
+            return Err(InternalError::with_message(response.text().map_err(
+                |_| {
+                    InternalError::with_message(
+                        "Unable to convert error response to text".to_string(),
+                    )
+                },
+            )?));
+        }
+
+        let batch_link = response.json::<BatchStatusLink>().map_err(|err| {
+            InternalError::from_source_with_message(
+                Box::new(err),
+                "Unable to get batch status link from response".to_string(),
+            )
+        })?;
+
+        let params: Vec<&str> = batch_link.link.split('?').collect();
+
+        let id_param: Vec<&str> = params[1].split('=').collect();
+
+        let id = id_param[1];
+
+        info!("Submitted batch: {}", id);
+
+        while wait_time > 0 {
+            let time = Instant::now();
+
+            let url = if let Some(service_id) = service_id {
+                format!(
+                    "{}&wait={}&service_id={}",
+                    batch_link.link, wait_time, service_id
+                )
+            } else {
+                format!("{}&wait={}", batch_link.link, wait_time)
+            };
+
+            let response = client.get(&url).send().map_err(|err| {
+                InternalError::from_source_with_message(
+                    Box::new(err),
+                    "Unable to get batch status".to_string(),
+                )
+            })?;
+
+            if !response.status().is_success() {
+                return Err(InternalError::with_message(response.text().map_err(
+                    |_| {
+                        InternalError::with_message(
+                            "Unable to convert error response to text".to_string(),
+                        )
+                    },
+                )?));
+            }
+
+            let batch_status = response.json::<BatchStatusResponse>().map_err(|err| {
+                InternalError::from_source_with_message(
+                    Box::new(err),
+                    "Unable to get batch status response".to_string(),
+                )
+            })?;
+
+            for t in &batch_status.data {
+                if t.status == "Invalid" {
+                    for i in &t.invalid_transactions {
+                        error!(
+                            "Error: {}",
+                            i.get("message")
+                                .unwrap_or(&"Batch contained invalid transactions".to_string())
+                        );
+                    }
+                }
+            }
+
+            if batch_status.data.iter().all(|d| d.status == "Valid") {
+                info!("Batch and transaction structure was valid. Batch queued.");
+            }
+
+            if batch_status.data.iter().all(|x| x.status != "PENDING") {
+                break;
+            }
+
+            wait_time -= time.elapsed().as_secs()
+        }
+
+        Ok(())
     }
 
     /// Retrieves the purchase order with the specified `id`.
@@ -99,4 +224,22 @@ impl Client for ReqwestClient {
     ) -> Result<Vec<PurchaseOrderRevision>, InternalError> {
         unimplemented!()
     }
+}
+
+#[derive(Deserialize, Debug)]
+pub struct BatchStatusLink {
+    pub link: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct BatchStatusResponse {
+    pub data: Vec<BatchStatus>,
+    pub link: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct BatchStatus {
+    pub id: String,
+    pub invalid_transactions: Vec<HashMap<String, String>>,
+    pub status: String,
 }
