019f54adf101c1abe0315da219f833c3c8911f31
Andrea Gunderson <agunde@bitwise.io>
2021-05-07 11:18:33 -0500

    Fix manual implementation of Option::map lint
    This change fixes a lint introduced in the 1.52.0 release of Rust
    flagging items like
    
        match Some(0) {
            Some(x) => Some(x + 1),
            None => None,
        };
    
    Use instead:
    
        Some(0).map(|x| x + 1);
    
    In some cases this requires an `as &(dyn Error + 'static)`.
    
    See for
        https://rust-lang.github.io/rust-clippy/master/index.html#manual_map
    for details
    
    Signed-off-by: Andrea Gunderson <agunde@bitwise.io>

--- sdk/src/hex.rs
+++ sdk/src/hex.rs
@@ -106,11 +106,9 @@ pub struct HexError {
 
 impl Error for HexError {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
-        if let Some(ref err) = self.source {
-            Some(&**err)
-        } else {
-            None
-        }
+        self.source
+            .as_ref()
+            .map(|err| &**err as &(dyn std::error::Error + 'static))
     }
 }
 
