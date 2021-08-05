36e8771c65695fd1e68d2781c66414fb7995dd44
Shannyn Telander <telander@bitwise.io>
2021-08-05 12:54:46 -0500

    Add documentation for `BuilderError`
    This change adds API documentation for the `BuilderError` used within
    the protocol module.
    
    Signed-off-by: Shannyn Telander <telander@bitwise.io>

--- sdk/src/protocol/errors.rs
+++ sdk/src/protocol/errors.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//! Provides error definitions encountered when building an object
+
 #[derive(Debug)]
 pub enum BuilderError {
     MissingField(String),
