43bf722e4c75f2b88c12b5488ce9e0fbd1938209
Kevin Johnson <kevin_johnson@cargill.com>
2021-08-26 10:25:04 -0500

    Create top-level traits for sdk client
    Creating client factory trait, ClientError and initial structures and
    top-level traits for location, pike, product, purchase_order, and schema
    clients.
    
    Signed-off-by: Kevin Johnson <kevin_johnson@cargill.com>

---
+++ sdk/src/client/location/mod.rs
@@ -0,0 +1,60 @@
+// Copyright 2021 Cargill Incorporated
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::error::ClientError;
+
+use super::{schema::DataType, Client};
+
+pub struct Location {
+    pub location_id: String,
+    pub location_namespace: String,
+    pub owner: String,
+    pub properties: Vec<LocationPropertyValue>,
+    pub service_id: Option<String>,
+}
+
+pub struct LocationPropertyValue {
+    pub name: String,
+    pub data_type: DataType,
+    pub service_id: Option<String>,
+    pub bytes_value: Option<Vec<u8>>,
+    pub boolean_value: Option<bool>,
+    pub number_value: Option<i64>,
+    pub string_value: Option<String>,
+    pub enum_value: Option<i32>,
+    pub struct_values: Option<Vec<String>>,
+    pub lat_long_value: Option<LatLong>,
+}
+
+pub struct LatLong {
+    pub latitude: i64,
+    pub longitude: i64,
+}
+
+pub trait LocationClient: Client {
+    /// Fetches an agent based on its identified
+    ///
+    /// # Arguments
+    ///
+    /// * `id` - the location's identifier
+    /// * `service_id` - optional - the service id to fetch the location from
+    fn get_location(&self, id: String, service_id: Option<&str>) -> Result<Location, ClientError>;
+
+    /// Fetches locations.
+    ///
+    /// # Arguments
+    ///
+    /// * `service_id` - optional - the service id to fetch locations from
+    fn list_locations(&self, service_id: Option<&str>) -> Result<Vec<Location>, ClientError>;
+}
--- sdk/src/client/mod.rs
+++ sdk/src/client/mod.rs
@@ -14,134 +14,50 @@
 
 //! Traits and implementations useful for interacting with the REST API.
 
-#[cfg(feature = "client-reqwest")]
-mod reqwest;
+pub mod location;
+pub mod pike;
+pub mod product;
+#[cfg(feature = "purchase-order")]
+pub mod purchase_order;
+pub mod schema;
 
+use crate::error::ClientError;
 use sawtooth_sdk::messages::batch::BatchList;
-use serde::{Deserialize, Serialize};
-use std::time::SystemTime;
-
-use crate::error::InternalError;
 
 pub trait Client {
     /// Submits a list of batches
     ///
     /// # Arguments
     ///
-    /// * `batches` - The `BatchList` to be submitted
+    /// * `wait` - wait time in seconds
+    /// * `batch_list` - The `BatchList` to be submitted
+    /// * `service_id` - optional service id if running splinter
     fn post_batches(
         &self,
+        wait: u64,
         batch_list: &BatchList,
         service_id: Option<&str>,
-        wait: u64,
-    ) -> Result<(), InternalError>;
-
-    /// Retrieves the purchase order with the specified `id`.
-    ///
-    /// # Arguments
-    ///
-    /// * `id` - The uuid of the `PurchaseOrder` to be retrieved
-    #[cfg(feature = "purchase-order")]
-    fn get_purchase_order(&self, id: String) -> Result<Option<PurchaseOrder>, InternalError>;
-
-    /// Retrieves the purchase order version with the given `version_id` of the purchase
-    /// order with the given `id`
-    ///
-    /// # Arguments
-    ///
-    /// * `id` - The uuid of the `PurchaseOrder` containing the `PurchaseOrderVersion` to be retrieved
-    /// * `version_id` - The version id of the `PurchaseOrderVersion` to be retrieved
-    #[cfg(feature = "purchase-order")]
-    fn get_purchase_order_version(
-        &self,
-        id: String,
-        version_id: String,
-    ) -> Result<Option<PurchaseOrderVersion>, InternalError>;
-
-    /// Retrieves the purchase order revision with the given `revision_id` of
-    /// the purchase order version with the given `version_id` of the purchase order with the given `id`
-    ///
-    /// # Arguments
-    ///
-    /// * `id` - The uuid of the `PurchaseOrder` containing the `PurchaseOrderRevision` to be retrieved
-    /// * `version_id` - The version id of the `PurchaseOrderVersion` containing the
-    ///   `PurchaseOrderRevision` to be retrieved
-    /// * `revision_id` - The revision number of the `PurchaseOrderRevision` to be retrieved
-    #[cfg(feature = "purchase-order")]
-    fn get_purchase_order_revision(
-        &self,
-        id: String,
-        version_id: String,
-        revision_id: String,
-    ) -> Result<Option<PurchaseOrderRevision>, InternalError>;
-
-    /// lists purchase orders.
-    ///
-    /// # Arguments
-    ///
-    /// * `filter` - Filter to apply to the list of `PurchaseOrder`s
-    #[cfg(feature = "purchase-order")]
-    fn list_purchase_orders(
-        &self,
-        filter: Option<&str>,
-    ) -> Result<Vec<PurchaseOrder>, InternalError>;
-
-    /// lists the purchase order versions of a specific purchase order.
-    ///
-    /// # Arguments
-    ///
-    /// * `id` - The uuid of the `PurchaseOrder` containing the `PurchaseOrderVersion`s to be listed
-    /// * `filter` - Filter to apply to the list of purchase orders
-    #[cfg(feature = "purchase-order")]
-    fn list_purchase_order_versions(
-        &self,
-        id: String,
-        filter: Option<&str>,
-    ) -> Result<Vec<PurchaseOrderVersion>, InternalError>;
-
-    /// lists the purchase order revisions of a specific purchase order version.
-    ///
-    /// # Arguments
-    ///
-    /// * `id` - The uuid of the `PurchaseOrder` containing the `PurchaseOrderRevision`s to be listed
-    /// * `version_id` - The version id of the `PurchaseOrderVersion` containing
-    ///   the `PurchaseOrderRevision`s to be listed
-    #[cfg(feature = "purchase-order")]
-    fn list_purchase_order_revisions(
-        &self,
-        id: String,
-        version_id: String,
-        filter: Option<&str>,
-    ) -> Result<Vec<PurchaseOrderRevision>, InternalError>;
+    ) -> Result<(), ClientError>;
 }
 
-#[cfg(feature = "purchase-order")]
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct PurchaseOrder {
-    org_id: String,
-    uuid: String,
-    workflow_status: String,
-    is_closed: bool,
-    accepted_version_id: Option<String>,
-    versions: Vec<PurchaseOrderVersion>,
-    created_at: SystemTime,
-}
+pub trait ClientFactory {
+    /// Retrieves a client for listing and showing locations
+    fn get_location_client(&self, url: String) -> Box<dyn location::LocationClient>;
 
-#[cfg(feature = "purchase-order")]
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct PurchaseOrderVersion {
-    version_id: String,
-    workflow_status: String,
-    is_draft: bool,
-    current_revision_id: u64,
-    revisions: Vec<PurchaseOrderRevision>,
-}
+    /// Retrieves a client for listing and showing pike members
+    fn get_pike_client(&self, url: String) -> Box<dyn pike::PikeClient>;
 
-#[cfg(feature = "purchase-order")]
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct PurchaseOrderRevision {
-    revision_id: u64,
-    order_xml_v3_4: String,
-    submitter: String,
-    created_at: u64,
+    /// Retrieves a client for listing and showing products
+    fn get_product_client(&self, url: String) -> Box<dyn product::ProductClient>;
+
+    /// Retrieves a client for listing and showing
+    /// purchase orders, revisions, and versions
+    #[cfg(feature = "purchase-order")]
+    fn get_purchase_order_client(
+        &self,
+        url: String,
+    ) -> Box<dyn purchase_order::PurchaseOrderClient>;
+
+    /// Retrieves a client for listing and showing schemas
+    fn get_schema_client(&self, url: String) -> Box<dyn schema::SchemaClient>;
 }
