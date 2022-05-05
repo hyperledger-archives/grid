d63b353d6feb69c40adbc20ba2a56c62953a07f3
Chris Eckhardt <eckhardt@bitwise.io>
2022-05-05 11:07:35 -0500

    Add `batch_submission` mod and `url_resolver`
    Adds the url_resolver submitter subcomponent and the batch_submission
    module structure that contains it.
    
    Resolves: #1414
    
    Signed-off-by: Chris Eckhardt <eckhardt@bitwise.io>

---
+++ sdk/src/batch_submission/submission/url_resolver/mod.rs
@@ -0,0 +1,22 @@
+// Copyright 2022 Cargill Incorporated
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::scope_id::ScopeId;
+
+/// An interface for generating the url to which a batch should be sent.
+pub trait UrlResolver: Sync + Send {
+    type Id: ScopeId;
+    /// Generates an address (i.e. URL) to which the batch will be sent.
+    fn url(&self, scope_id: &Self::Id) -> String;
+}
