becae180af724624838c25ba7b11cab90d30c45c
Chris Eckhardt <eckhardt@bitwise.io>
2022-06-01 10:49:46 -0500

    Add submitter traits and module
    This defines two interfaces: a non-blocking submitter that has been built but
    is not yet running and a running submitter service.
    
    Signed-off-by: Chris Eckhardt <eckhardt@bitwise.io>

--- sdk/src/batch_submission/submission/mod.rs
+++ sdk/src/batch_submission/submission/mod.rs
@@ -12,5 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+pub mod submitter;
 pub mod submitter_observer;
 pub mod url_resolver;
---
+++ sdk/src/batch_submission/submission/submitter/mod.rs
@@ -0,0 +1,31 @@
+// Copyright 2022 Cargill Incorporated
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::{error::InternalError, scope_id::ScopeId, threading::lifecycle::ShutdownHandle};
+
+/// The interface for a non-blocking submitter that is built but not yet running.
+pub trait RunnableSubmitter<S: ScopeId> {
+    type RunningSubmitter: RunningSubmitter<S>;
+
+    /// Start running the submission service.
+    fn run(self) -> Result<Self::RunningSubmitter, InternalError>;
+}
+
+/// The interface for a running submitter.
+pub trait RunningSubmitter<S: ScopeId>: ShutdownHandle {
+    type RunnableSubmitter: RunnableSubmitter<S>;
+
+    /// Stop the running submitter service and return a runnable submitter (pause the service).
+    fn stop(self) -> Result<Self::RunnableSubmitter, InternalError>;
+}
