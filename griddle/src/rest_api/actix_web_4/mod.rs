dadda79256af24a87aba319378b10802c1ee919d
Shannyn Telander <telander@bitwise.io>
2022-06-09 08:42:56 -0500

    Add builder for Griddle REST API
    This change adds a builder struct to create a runnable version of the
    Griddle REST API. An initial version of the `RunnableGriddleRestApi` is
    also added in this change.
    
    Signed-off-by: Shannyn Telander <telander@bitwise.io>

--- griddle/src/rest_api/actix_web_4/mod.rs
+++ griddle/src/rest_api/actix_web_4/mod.rs
@@ -12,6 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+mod builder;
+mod runnable;
+
+pub use builder::GriddleRestApiBuilder;
+pub use runnable::RunnableGriddleRestApi;
+
 use actix_web::Resource;
 
 /// A `ResourceProvider` provides a list of resources.
---
+++ griddle/src/rest_api/actix_web_4/runnable.rs
@@ -0,0 +1,32 @@
+// Copyright 2018-2022 Cargill Incorporated
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Contains the implementation of `RunnableRestApi`
+
+use cylinder::Signer;
+#[cfg(feature = "proxy")]
+use grid_sdk::proxy::ProxyClient;
+use grid_sdk::rest_api::actix_web_4::Endpoint;
+
+use crate::rest_api::actix_web_4::GriddleResourceProvider;
+
+/// A configured REST API which may best started with `run` function.
+pub struct RunnableGriddleRestApi {
+    pub(super) resource_providers: Vec<Box<dyn GriddleResourceProvider>>,
+    pub(super) bind: String,
+    #[cfg(feature = "proxy")]
+    pub(super) proxy_client: Box<dyn ProxyClient>,
+    pub(super) signer: Box<dyn Signer>,
+    pub(super) dlt_backend_endpoint: Endpoint,
+}
